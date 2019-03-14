51cc5f19d6c71b981ab4a589e98135e9082be329
Christian Williams <antixian666@gmail.com>
2019-03-14 00:52:23 -0700

    Add ColorPicker.

--- src/commonMain/kotlin/baaahs/Show.kt
+++ src/commonMain/kotlin/baaahs/Show.kt
@@ -58,7 +58,7 @@ interface Show {
 }
 
 class SomeDumbShow(sheepModel: SheepModel, showRunner: ShowRunner) : Show {
-    //    val colorPicker = showRunner.getColorPicker()
+    val colorPicker = showRunner.getColorPicker()
 //    val panelShaderBuffers = sheepModel.allPanels.map { showRunner.getSolidShaderBuffer(it) }
     val pixelShaderBuffers = sheepModel.allPanels.map { showRunner.getPixelShaderBuffer(it) }
 
@@ -68,23 +68,15 @@ class SomeDumbShow(sheepModel: SheepModel, showRunner: ShowRunner) : Show {
 
     override fun nextFrame() {
 //        panelShaderBuffers.forEach { shaderBuffer -> shaderBuffer.color = Color.random() }
+        val seed = Random(0)
 
         pixelShaderBuffers.forEach { shaderBuffer ->
-            shaderBuffer.colors.forEachIndexed { i, pixel -> shaderBuffer.colors[i] = Color.random() }
-        }
-    }
-
-    fun nextFrame(color: Color?, beat: Int, brains: MutableMap<Network.Address, RemoteBrain>, link: Network.Link) {
-        brains.values.forEach { brain ->
-            val brainSeed = brain.address.toString().hashCode()
-            val saturation = Random(brainSeed).nextFloat() *
-                    abs(sin(brainSeed + getTimeMillis() / 1000.toDouble())).toFloat()
-            val desaturatedColor = color!!.withSaturation(saturation)
-            link.send(
-                brain.address,
-                Ports.BRAIN,
-                BrainShaderMessage(SolidShaderBuffer().also { it.color = desaturatedColor })
-            )
+            shaderBuffer.colors.forEachIndexed { i, pixel ->
+                val saturation = seed.nextFloat() *
+                        abs(sin(seed.nextDouble() + getTimeMillis() / 1000.toDouble())).toFloat()
+                val desaturatedColor = colorPicker.color.withSaturation(saturation)
+                shaderBuffer.colors[i] = desaturatedColor
+            }
         }
     }
 }
