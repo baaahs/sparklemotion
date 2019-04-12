a2e7cb023c87d411d6b6fe918f9dcb2d440ba84c
Christian Williams <antixian666@gmail.com>
2019-04-12 01:59:55 -0700

    Add more tests for Color.

--- src/commonTest/kotlin/baaahs/ColorTest.kt
+++ src/commonTest/kotlin/baaahs/ColorTest.kt
@@ -21,4 +21,23 @@ public class ColorTest {
         assertEquals(1f, Color.WHITE.distanceTo(Color.BLACK))
         assertEquals(0f, Color.WHITE.distanceTo(Color.WHITE))
     }
+
+    @Test
+    fun testToHexString() {
+        assertEquals("#fefdff", Color.from("#fefdff").toHexString())
+    }
+
+    @Test
+    fun testWithSaturation() {
+        val red = Color.from("#ff0000")
+        assertEquals(red, red.withSaturation(1f))
+        assertEquals(Color.from("#ff7f7f"), red.withSaturation(.5f))
+    }
+
+    @Test
+    fun testFade() {
+        assertEquals(Color.RED, Color.RED.fade(Color.GREEN, 0f))
+        assertEquals(Color.GREEN, Color.RED.fade(Color.GREEN, 1f))
+        assertEquals(Color.from("#7f7f00"), Color.RED.fade(Color.GREEN, 0.5f))
+    }
 }
