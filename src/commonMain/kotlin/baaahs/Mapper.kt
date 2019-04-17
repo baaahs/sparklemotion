064ae67ca3e60a96d489a4dfa470554d533bfe2d
Christian Williams <antixian666@gmail.com>
2019-04-17 14:41:27 -0700

    Render mapper UI, including simulated camera and wireframe overlay.

--- src/commonMain/kotlin/baaahs/Mapper.kt
+++ src/commonMain/kotlin/baaahs/Mapper.kt
@@ -5,7 +5,21 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class Mapper(val network: Network, val display: MapperDisplay) : Network.UdpListener {
+class Mapper(
+    val network: Network,
+    val sheepModel: SheepModel,
+    mediaDevices: MediaDevices
+) : Network.UdpListener {
+    val width = 640
+    val height = 300
+    val mapperDisplay = MapperDisplay(sheepModel, { onClose() })
+    val camera = mediaDevices.getCamera(width, height).apply {
+        onImage = this@Mapper::haveImage
+    }
+    val baseBitmap = UByteArray(width * height * 4)
+    val displayBitmap = UByteArray(width * height * 4)
+
+    private val closeListeners = mutableListOf<() -> Unit>()
     private lateinit var link: Network.Link
     private var isRunning: Boolean = false
 
@@ -13,22 +27,25 @@ class Mapper(val network: Network, val display: MapperDisplay) : Network.UdpList
         link = network.link()
         link.listenUdp(Ports.MAPPER, this)
 
-        display.onStart = {
-            if (!isRunning) {
-                isRunning = true
+        isRunning = true
 
-                GlobalScope.launch {
-                    run()
-                }
-            }
+        GlobalScope.launch {
+            run()
         }
+    }
 
-        display.onStop = {
-            if (isRunning) {
-                isRunning = false
-            }
-        }
+    private fun onClose() {
+        camera.close()
+        isRunning = false
+        closeListeners.forEach { it.invoke() }
+    }
 
+    private fun haveImage(image: MediaDevices.Image) {
+//        println("image: $image")
+        mapperDisplay.showCamImage(image)
+
+        val toBitmap = image.toMonoBitmap()
+//        println("toMonoBitmap = ${toMonoBitmap}")
     }
 
     suspend fun run() {
@@ -70,4 +87,11 @@ class Mapper(val network: Network, val display: MapperDisplay) : Network.UdpList
         }
     }
 
+    fun addCloseListener(listener: () -> Unit) {
+        closeListeners.add(listener)
+    }
+}
+
+expect class MapperDisplay(sheepModel: SheepModel, onExit: () -> Unit) {
+    fun showCamImage(image: MediaDevices.Image)
 }
