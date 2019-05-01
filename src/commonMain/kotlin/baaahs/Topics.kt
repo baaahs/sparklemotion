78ca6acab5252503b7a74ea37f0adf1885749432
Christian Williams <antixian666@gmail.com>
2019-04-30 22:40:02 -0700

    Adds react UI for available shows, displays currently selected show

--- src/commonMain/kotlin/baaahs/Topics.kt
+++ src/commonMain/kotlin/baaahs/Topics.kt
@@ -1,5 +1,9 @@
 package baaahs
 
+import kotlinx.serialization.serializer
+
 object Topics {
+    val availableShows = PubSub.Topic("availableShows", String.serializer())
+    val selectedShow = PubSub.Topic("selectedShow", String.serializer())
     val primaryColor = PubSub.Topic("primaryColor", Color.serializer())
 }
