46ff23f88b3076ec9a6c73b036459a9359b223cc
Chris Hasson <hassoncs@gmail.com>
2019-06-06 23:24:04 -0400

    Update slider gadget style
    And make it vertical. This needs to be cleaned up a bit–
    splitting out the styles into css modules and such.
    
    But it’s an improvement!

--- src/jsMain/js/app/Slider/index.jsx
+++ src/jsMain/js/app/Slider/index.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import sass from './Slider.scss';
+import { Slider, Handles, Tracks, Rail, Ticks } from 'react-compound-slider';
+import { Handle, Track, Tick, SliderRail } from './slider-parts';
 
-class Slider extends React.Component {
+const sliderStyle = {
+  position: 'relative',
+  height: '200px',
+  marginLeft: '45%',
+  touchAction: 'none',
+};
+
+const domain = [0, 1];
+
+class RangeSlider extends React.Component {
   constructor(props) {
     super(props);
 
+    let values = [props.gadget.value];
     this.state = {
       gadget: props.gadget,
+      values: values,
+      update: values,
     };
 
-    props.gadget.listen({onChanged: () => {this.forceUpdate()}});
+    props.gadget.listen({
+      onChanged: () => {
+        this.forceUpdate();
+      },
+    });
   }
 
-  componentDidMount() {
-  }
+  onUpdate = (update) => {
+    this.setState({ update });
+  };
+
+  onChange = (values) => {
+    const { gadget } = this.props;
+    this.setState({ values });
+    gadget.value = 1 - values[0];
+  };
 
   handleSliderChange = (event) => {
     const { gadget } = this.state;
@@ -23,36 +48,77 @@ class Slider extends React.Component {
   };
 
   render() {
-    const { gadget } = this.state;
+    const { gadget, values, update } = this.state;
 
     return (
       <div className={sass['slider--wrapper']}>
         <label className={sass['slider--label']} htmlFor="range-slider">
-          {gadget.name}: {gadget.value}
+          {gadget.name}
         </label>
-        <input
-          type="range"
-          id="start"
-          name="range-slider"
-          min="0"
-          max="1"
-          value={gadget.value}
-          step=".01"
-          onChange={this.handleSliderChange.bind(this)}
-        />
+        <Slider
+          vertical
+          mode={2}
+          step={0.01}
+          domain={domain}
+          rootStyle={sliderStyle}
+          onUpdate={this.onUpdate}
+          onChange={this.onChange}
+          values={values}
+        >
+          <Rail>
+            {({ getRailProps }) => <SliderRail getRailProps={getRailProps} />}
+          </Rail>
+          <Handles>
+            {({ handles, getHandleProps }) => (
+              <div className="slider-handles">
+                {handles.map((handle) => (
+                  <Handle
+                    key={handle.id}
+                    handle={handle}
+                    domain={domain}
+                    getHandleProps={getHandleProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Handles>
+          <Tracks left={false} right={false}>
+            {({ tracks, getTrackProps }) => (
+              <div className="slider-tracks">
+                {tracks.map(({ id, source, target }) => (
+                  <Track
+                    key={id}
+                    source={source}
+                    target={target}
+                    getTrackProps={getTrackProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Tracks>
+          <Ticks count={5}>
+            {({ ticks }) => (
+              <div className="slider-ticks">
+                {ticks.map((tick) => (
+                  <Tick key={tick.id} tick={tick} />
+                ))}
+              </div>
+            )}
+          </Ticks>
+        </Slider>
       </div>
     );
   }
 }
 
-Slider.propTypes = {
+RangeSlider.propTypes = {
   pubSub: PropTypes.object,
-//   gadget: PropTypes.object,
+  //   gadget: PropTypes.object,
 };
 
-Slider.defaultProps = {
+RangeSlider.defaultProps = {
   pubSub: {},
-//   gadget: {},
+  //   gadget: {},
 };
 
-export default Slider;
+export default RangeSlider;
