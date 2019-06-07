285727e310bb8e6de0c8a3621fabd4a5bce9b525
Chris Hasson <hassoncs@gmail.com>
2019-06-07 00:03:12 -0400

    WIP: Tabify the FakeClientDevice

--- src/jsMain/js/app/components/NavigationTabBar/constants.js
+++ src/jsMain/js/app/components/NavigationTabBar/constants.js
@@ -1,9 +1,8 @@
-export const TAB_OPTION_SHOW_LIST = 'shows';
-export const TAB_OPTION_SHOW_INTERFACE = 'show ui';
+export const TAB_OPTION_SHOW_LIST = 'show picker';
+export const TAB_OPTION_SHOW_INTERFACE = 'controls';
 export const TAB_OPTION_COLOR_PICKER = 'color';
 
 export const WEB_UI_TABS_LIST = [
   TAB_OPTION_SHOW_LIST,
   TAB_OPTION_SHOW_INTERFACE,
-  TAB_OPTION_COLOR_PICKER,
 ];
--- src/jsMain/js/app/components/TabContent/index.jsx
+++ src/jsMain/js/app/components/TabContent/index.jsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   TAB_OPTION_SHOW_LIST,
   TAB_OPTION_SHOW_INTERFACE,
-  TAB_OPTION_COLOR_PICKER,
   WEB_UI_TABS_LIST,
 } from '../NavigationTabBar/constants';
+import ShowList from '../../ShowList';
+import RangeSlider from '../../Slider';
+import styles from '../../app.scss';
+import ColorPicker from "../../Menu/components/ColorPicker";
 
-const TabContent = ({ selectedTab }) => {
+class ControlsTabContent extends Component {
+  render() {
+    const { gadgets } = this.props;
+
+    return (
+      <div className={styles.root}>
+        {gadgets.map((gadgetInfo) => {
+          const { gadget, topicName } = gadgetInfo;
+          return (
+            <div key={topicName} className={styles['gadget-view']}>
+              {this.createGadgetView(gadget)}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  createGadgetView = (gadget) => {
+    if (gadget instanceof baaahs.gadgets.ColorPicker) {
+      return <ColorPicker gadget={gadget} />;
+    } else if (gadget instanceof baaahs.gadgets.Slider) {
+      return <RangeSlider gadget={gadget} />;
+    } else {
+      return <div />;
+    }
+  };
+}
+
+const ShowsTabContent = (props) => {
+  return <ShowList pubSub={props.pubSub} />;
+};
+
+const TabContent = (props) => {
+  const { selectedTab } = props;
+  debugger;
   switch (selectedTab) {
     case TAB_OPTION_SHOW_LIST:
-      return <div>TODO: tab content</div>;
+      return <ShowsTabContent {...props} />;
     case TAB_OPTION_SHOW_INTERFACE:
-      return <div>TODO: Show interface tab</div>;
-    case TAB_OPTION_COLOR_PICKER:
-      return <div>TODO: COlor picker tabbbbbb</div>;
+      return <ControlsTabContent {...props} />;
     default:
       return null;
   }
