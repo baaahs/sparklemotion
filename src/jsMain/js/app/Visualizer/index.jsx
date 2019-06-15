4ee6eca6387c067db325a91d65708b24bec1a921
Christian Williams <antixian666@gmail.com>
2019-06-15 10:38:41 -0700

    WIP

---
+++ src/jsMain/js/app/Visualizer/index.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import styles from './ColorPicker.scss';
+import classNames from 'classnames';
+
+const baaahs = sparklemotion.baaahs;
+
+class Visualizer extends Component {
+  constructor(props) {
+    super(props);
+
+    new baaahs.visualizer.Visualizer()
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+
+  }
+}
