3f321505e933d2d3154f4076330de2593ae1ea65
Christian Williams <antixian666@gmail.com>
2019-06-19 21:00:15 -0700

    Display pixels as texture-mapped quads to give them some volume.
    - Adjust spacing between pixels for better panel coverage.
    - Add BufferGeometryUtils from
      https://github.com/mrdoob/three.js/commit/702fdf7928b5c78a14952377568a3287c46db1ca#diff-ed66fde795b2b6bbde357c48f101328d

--- src/jsMain/kotlin/three/three-extras.kt
+++ src/jsMain/kotlin/three/three-extras.kt
@@ -1,10 +1,21 @@
 @file:JsQualifier("THREE")
+
 package three
 
 import info.laht.threekt.core.BufferAttribute
+import info.laht.threekt.core.BufferGeometry
 
 open external class Float32BufferAttribute(
     array: dynamic,
     itemSize: Int,
     normalized: Boolean = definedExternally
 ) : BufferAttribute
+
+open external class BufferGeometryUtils() {
+    companion object {
+        fun mergeBufferGeometries(
+            geometries: Array<BufferGeometry>,
+            useGroups: Boolean = definedExternally
+        ): BufferGeometry
+    }
+}
