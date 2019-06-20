42a6a9f31f4c058957211f04e85df38ad620e877
Christian Williams <antixian666@gmail.com>
2019-06-19 19:47:44 -0700

    Minor debug cleanup.

--- src/commonMain/kotlin/baaahs/GadgetManager.kt
+++ src/commonMain/kotlin/baaahs/GadgetManager.kt
@@ -39,9 +39,11 @@ class GadgetManager(private val pubSub: PubSub.Server) {
                 }
             }
         } else {
-            println("Gadgets don't match!")
-            println("old: ${priorRequestedGadgets}")
-            println("new: ${requestedGadgets}")
+            logger.debug(
+                "Gadgets don't match!\n" +
+                        "old: ${priorRequestedGadgets}\n" +
+                        "new: ${requestedGadgets}"
+            )
             activeGadgets.clear()
             requestedGadgets.forEach { (name, gadget) ->
                 val gadgetId = nextGadgetId++
