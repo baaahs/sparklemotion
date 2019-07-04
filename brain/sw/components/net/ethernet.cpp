d46b7a83b1cb4b1c19a8904ccdd28db62aae0ebf
Tom Seago <tom@tomseago.com>
2019-07-04 04:32:35 -0700

    Updates related to using the absolutely latest IDF

--- brain/sw/components/net/ethernet.cpp
+++ brain/sw/components/net/ethernet.cpp
@@ -2,86 +2,14 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_event_loop.h"
 #include "esp_err.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_eth.h"
-#include "esp32/rom/gpio.h"
 #include "tcpip_adapter.h"
-#include "driver/gpio.h"
-#include "driver/periph_ctrl.h"
+#include "sdkconfig.h"
 
-#if CONFIG_PHY_LAN8720
-#include "eth_phy/phy_lan8720.h"
-#define DEFAULT_ETHERNET_PHY_CONFIG phy_lan8720_default_ethernet_config
-#elif CONFIG_PHY_TLK110
-#include "eth_phy/phy_tlk110.h"
-#define DEFAULT_ETHERNET_PHY_CONFIG phy_tlk110_default_ethernet_config
-#elif CONFIG_PHY_IP101
-#include "eth_phy/phy_ip101.h"
-#define DEFAULT_ETHERNET_PHY_CONFIG phy_ip101_default_ethernet_config
-#endif
-
-#define PIN_PHY_POWER CONFIG_PHY_POWER_PIN
-#define PIN_SMI_MDC CONFIG_PHY_SMI_MDC_PIN
-#define PIN_SMI_MDIO CONFIG_PHY_SMI_MDIO_PIN
-
-#ifdef CONFIG_PHY_USE_POWER_PIN
-/**
- * @brief re-define power enable func for phy
- *
- * @param enable true to enable, false to disable
- *
- * @note This function replaces the default PHY power on/off function.
- * If this GPIO is not connected on your device (and PHY is always powered),
- * you can use the default PHY-specific power on/off function.
- */
-static void phy_device_power_enable_via_gpio(bool enable)
-{
-    assert(DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable);
-
-    if (!enable) {
-        DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable(false);
-    }
-
-    gpio_pad_select_gpio(PIN_PHY_POWER);
-    gpio_set_direction((gpio_num_t)PIN_PHY_POWER, (gpio_mode_t)GPIO_MODE_OUTPUT);
-    if (enable == true) {
-        gpio_set_level((gpio_num_t)PIN_PHY_POWER, 1);
-        ESP_LOGI(TAG, "Power On Ethernet PHY");
-    } else {
-        gpio_set_level((gpio_num_t)PIN_PHY_POWER, 0);
-        ESP_LOGI(TAG, "Power Off Ethernet PHY");
-    }
-
-    vTaskDelay(1); // Allow the power up/down to take effect, min 300us
-
-    if (enable) {
-        /* call the default PHY-specific power on function */
-        DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable(true);
-    }
-}
-#endif
-
-/**
- * @brief gpio specific init
- *
- * @note RMII data pins are fixed in esp32:
- * TXD0 <=> GPIO19
- * TXD1 <=> GPIO22
- * TX_EN <=> GPIO21
- * RXD0 <=> GPIO25
- * RXD1 <=> GPIO26
- * CLK <=> GPIO0
- *
- */
-static void eth_gpio_config_rmii(void)
-{
-    phy_rmii_configure_data_interface_pins();
-    phy_rmii_smi_configure_pins(PIN_SMI_MDC, PIN_SMI_MDIO);
-}
+static esp_eth_handle_t s_eth_handle = NULL;
 
 /** Event handler for Ethernet events */
 static void eth_event_handler(void* arg, esp_event_base_t event_base, 
@@ -124,21 +52,32 @@ void ethernet_init()
 {
     ESP_LOGD(TAG, "ethernet_init start");
 
-    eth_config_t config = DEFAULT_ETHERNET_PHY_CONFIG;
-    config.phy_addr = (eth_phy_base_t)CONFIG_PHY_ADDRESS;
-    config.gpio_config = eth_gpio_config_rmii;
-    config.tcpip_input = tcpip_adapter_eth_input;
-    config.clock_mode = (eth_clock_mode_t)CONFIG_PHY_CLOCK_MODE;
-#ifdef CONFIG_PHY_USE_POWER_PIN
-    /* Replace the default 'power enable' function with an example-specific one
-     that toggles a power GPIO. */
-    config.phy_power_enable = phy_device_power_enable_via_gpio;
-#endif
-
-    ESP_ERROR_CHECK(esp_eth_init(&config));
+    ESP_ERROR_CHECK(tcpip_adapter_set_default_eth_handlers());
     ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_eth_enable()) ;
+
+    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&mac_config);
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+
+    // Because the default example is assuming one of those breakout boards that
+    // has the address set to 1 we have to be sure to set it to 0
+    phy_config.phy_addr = 0;
+
+#if CONFIG_PHY_IP101
+    esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
+#elif CONFIG_PHY_RTL8201
+    esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
+#elif CONFIG_PHY_LAN8720
+    esp_eth_phy_t *phy = esp_eth_phy_new_lan8720(&phy_config);
+#elif CONFIG_PHY_DP83848
+    esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
+#endif
+
+    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
+
+    // The driver installation now calls init and the enable
+    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &s_eth_handle));
 
     ESP_LOGD(TAG, "ethernet_init done");
 }
