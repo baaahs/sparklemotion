af486da4f3c2de5182c5977437e47b2461fb8270
Tom Seago <tom@tomseago.com>
2019-07-11 01:47:49 -0700

    Adding basic button support

--- brain/sw/components/brain-ui/include/brain-ui.h
+++ brain/sw/components/brain-ui/include/brain-ui.h
@@ -1,19 +1,26 @@
 #pragma once
 
 #include <brain-led.h>
+#include <brain-button.h>
 
-class BrainUI {
+class BrainUI : public BrainButtonListener {
 public:
     BrainUI();
 
     void start();
     void _task();
 
+    void buttonDown(BrainButton& sw, bool longPress) override;
+    void buttonUp(BrainButton& sw) override;
+
 private:
-    BrainLed green;
-    BrainLed blue;
+    BrainLed leftEye;
+    BrainLed rightEye;
 
     BrainLed rgbR;
     BrainLed rgbG;
     BrainLed rgbB;
+
+    BrainButton btnLeft;
+    BrainButton btnRight;
 };
