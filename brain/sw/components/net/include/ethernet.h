681cf5f2ba4ff9204c615610cf4d0bf6a286e468
Tom Seago <tom@tomseago.com>
2019-07-16 18:12:14 -0700

    Working on refactoring network interface management stuff into classes

--- brain/sw/components/net/include/ethernet.h
+++ brain/sw/components/net/include/ethernet.h
@@ -1,6 +1,23 @@
-#ifndef BRAIN_ETHERNET_H
-#define BRAIN_ETHERNET_H
+#pragma once
 
-extern void ethernet_init();
+// Old
+//extern void ethernet_init();
 
-#endif
+// New
+
+#include "net_interface.h"
+#include "esp_event.h"
+#include "esp_eth.h"
+
+class Ethernet : public NetInterface {
+public:
+    Ethernet();
+
+    void start();
+    void _ethHandler(esp_event_base_t evBase, int32_t evId, void *evData);
+    void _ipHandler(esp_event_base_t evBase, int32_t evId, void *evData);
+
+private:
+    esp_eth_handle_t m_handle;
+
+};
