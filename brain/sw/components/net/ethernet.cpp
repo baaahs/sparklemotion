f72df86a4c8303d2264b26bab3865b4366af0ae2
Tom Seago <tom@tomseago.com>
2019-07-16 18:12:14 -0700

    Working on refactoring network interface management stuff into classes

--- brain/sw/components/net/ethernet.cpp
+++ brain/sw/components/net/ethernet.cpp
@@ -1,43 +1,89 @@
+#include "ethernet.h"
 #include "net_priv.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_err.h"
-#include "esp_event.h"
 #include "esp_log.h"
-#include "esp_eth.h"
 #include "tcpip_adapter.h"
 #include "sdkconfig.h"
 
-static esp_eth_handle_t s_eth_handle = NULL;
-
 /** Event handler for Ethernet events */
-static void eth_event_handler(void* arg, esp_event_base_t event_base, 
-                              int32_t event_id, void* event_data)
+void glue_ethHandler(void* pArg, esp_event_base_t evBase, int32_t evId, void* evData)
 {
-    switch (event_id) {
-    case ETHERNET_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "Ethernet Link Up");
-        break;
-    case ETHERNET_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "Ethernet Link Down");
-        break;
-    case ETHERNET_EVENT_START:
-        ESP_LOGI(TAG, "Ethernet Started");
-        break;
-    case ETHERNET_EVENT_STOP:
-        ESP_LOGI(TAG, "Ethernet Stopped");
-        break;
-    default:
-        break;
-    }
+    ((Ethernet *) pArg)->_ethHandler(evBase, evId, evData);
 }
 
 /** Event handler for IP_EVENT_ETH_GOT_IP */
-static void got_ip_event_handler(void* arg, esp_event_base_t event_base, 
-                                 int32_t event_id, void* event_data)
+void glue_ipHandler(void* pArg, esp_event_base_t evBase, int32_t evId, void* evData)
 {
-    ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+    ((Ethernet *) pArg)->_ipHandler(evBase, evId, evData);
+}
+
+
+Ethernet::Ethernet() {
+
+}
+
+void
+Ethernet::start() {
+    ESP_LOGD(TAG, "ethernet_init start");
+
+    ESP_ERROR_CHECK_WITHOUT_ABORT(tcpip_adapter_set_default_eth_handlers());
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, glue_ethHandler, NULL));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, glue_ipHandler, NULL));
+
+    eth_mac_config_t macConfig = ETH_MAC_DEFAULT_CONFIG();
+    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&macConfig);
+    eth_phy_config_t phyConfig = ETH_PHY_DEFAULT_CONFIG();
+
+    // Because the default example is assuming one of those 8720 breakout boards that
+    // has the address set to 1 we have to be sure to set it to 0
+    phyConfig.phy_addr = 0;
+
+    // We could remove these #if's for PHY's we aren't using but if someone uses
+    // our code on some other board they would just have to put them back
+#if CONFIG_PHY_IP101
+    esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phyConfig);
+#elif CONFIG_PHY_RTL8201
+    esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phyConfig);
+#elif CONFIG_PHY_LAN8720
+    esp_eth_phy_t *phy = esp_eth_phy_new_lan8720(&phyConfig);
+#elif CONFIG_PHY_DP83848
+    esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phyConfig);
+#endif
+
+    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
+
+    // The driver installation now calls init and the enable
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_eth_driver_install(&config, &m_handle));
+
+    ESP_LOGD(TAG, "ethernet_init done");
+}
+
+void
+Ethernet::_ethHandler(esp_event_base_t evBase, int32_t evId, void *evData) {
+    switch (evId) {
+        case ETHERNET_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "Ethernet Link Up");
+            break;
+        case ETHERNET_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "Ethernet Link Down");
+            break;
+        case ETHERNET_EVENT_START:
+            ESP_LOGI(TAG, "Ethernet Started");
+            break;
+        case ETHERNET_EVENT_STOP:
+            ESP_LOGI(TAG, "Ethernet Stopped");
+            break;
+        default:
+            break;
+    }
+}
+
+void
+Ethernet::_ipHandler(esp_event_base_t evBase, int32_t evId, void *evData) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*) evData;
     const tcpip_adapter_ip_info_t* ip_info = &event->ip_info;
 
     ESP_LOGI(TAG, "Ethernet Got IP Address");
@@ -46,38 +92,4 @@ static void got_ip_event_handler(void* arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
     ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
     ESP_LOGI(TAG, "~~~~~~~~~~~");
-}
-
-void ethernet_init()
-{
-    ESP_LOGD(TAG, "ethernet_init start");
-
-    ESP_ERROR_CHECK(tcpip_adapter_set_default_eth_handlers());
-    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-
-    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
-    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&mac_config);
-    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-
-    // Because the default example is assuming one of those breakout boards that
-    // has the address set to 1 we have to be sure to set it to 0
-    phy_config.phy_addr = 0;
-
-#if CONFIG_PHY_IP101
-    esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
-#elif CONFIG_PHY_RTL8201
-    esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
-#elif CONFIG_PHY_LAN8720
-    esp_eth_phy_t *phy = esp_eth_phy_new_lan8720(&phy_config);
-#elif CONFIG_PHY_DP83848
-    esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
-#endif
-
-    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
-
-    // The driver installation now calls init and the enable
-    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &s_eth_handle));
-
-    ESP_LOGD(TAG, "ethernet_init done");
 }
