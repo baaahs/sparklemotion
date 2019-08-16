b228e31d9b71690da3ddb9e8baea7fe25174888e
Chris Hasson <hassoncs@gmail.com>
2019-08-16 16:16:40 -0700

    All a bunch more GLSLSandbox shows
    Adds 10 more cool shows to choose from

---
+++ src/commonMain/kotlin/baaahs/shows/GlslSandbox46744Show.kt
@@ -0,0 +1,31 @@
+package baaahs.shows
+
+object GlslSandbox46744Show : GlslShow("GlslSandbox 46744") {
+
+    override val program = """
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float time;
+uniform vec2 resolution;
+
+float sq(float x) {
+	return x*x;
+}
+
+void main() {
+    vec2 p = gl_FragCoord.xy / resolution.x * 0.7;
+    vec3 col;
+    for(float j = 0.0; j < 3.0; j++){
+        for(float i = 1.0; i < 10.0; i++){
+            p.x += 0.1 / (i + j) * sin(i * 7.0 * p.y + time + cos((time / (1. * i)) * i + j));
+            p.y += 0.1 / (i + j)* cos(i * 1.0 * p.x + time + sin((time / (2. * i)) * i + j));
+        }
+        col[int(j)] = sin(75.0*sq(p.x)) + sin(75.0*sq(p.y));
+    }
+    gl_FragColor = vec4(col, 1.);
+}
+"""
+
+}
