d4575e1057dd77a030263822712572f520f9383a
Afik Cohen <aphex@aphex.cx>
2019-08-26 03:44:28 -0700

    Add brightness and saturation controls to solidcolor show and made available to glsl shows. Need to make all glsl shows use this automatically

--- src/commonMain/kotlin/baaahs/shows/SolidColorShow.kt
+++ src/commonMain/kotlin/baaahs/shows/SolidColorShow.kt
@@ -5,11 +5,14 @@ import baaahs.Model
 import baaahs.Show
 import baaahs.ShowRunner
 import baaahs.gadgets.ColorPicker
+import baaahs.gadgets.Slider
 import baaahs.shaders.SolidShader
 
 object SolidColorShow : Show("Solid Color") {
     override fun createRenderer(model: Model<*>, showRunner: ShowRunner): Renderer {
         val colorPicker = showRunner.getGadget("color", ColorPicker("Color"))
+        val saturationPicker = showRunner.getGadget("sm_saturation", Slider("Saturation"))
+        val brightnessPicker = showRunner.getGadget("sm_brightness", Slider("Brightness"))
 
         val shader = SolidShader()
         val shaderBuffers = showRunner.allSurfaces.map {
@@ -21,7 +24,9 @@ object SolidColorShow : Show("Solid Color") {
         return object : Renderer {
             override fun nextFrame() {
                 val color = colorPicker.color
-                shaderBuffers.forEach { it.color = color }
+                shaderBuffers.forEach {
+                    it.color = color.withSaturation(saturationPicker.value).withBrightness(brightnessPicker.value)
+                }
                 eyes.forEach { it.color = color }
             }
         }
