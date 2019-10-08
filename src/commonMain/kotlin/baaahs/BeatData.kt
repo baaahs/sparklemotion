191ab925358222cf56526bcd66602ffc7e9df8bd
Christian Williams <antixian666@gmail.com>
2019-10-07 22:38:22 -0700

    Change beat intensity to be sine wave.

--- src/commonMain/kotlin/baaahs/BeatData.kt
+++ src/commonMain/kotlin/baaahs/BeatData.kt
@@ -2,6 +2,10 @@ package baaahs
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
 
 @Serializable
 data class BeatData(
@@ -34,13 +38,24 @@ data class BeatData(
 
     /** Returns 1.0 if we're on a beat, 0.0 when we're furthest from the last beat,
      * and anywhere in between otherwise. */
-    fun fractionTillNextBeat(clock: Clock): Float =
-        if (beatIntervalMs == 0) -1f else 1 - beatWithinMeasure(clock) % 1.0f
+    fun fractionTillNextBeat(clock: Clock): Float {
+        return if (beatIntervalMs == 0) -1f else return clamp(sineWithEarlyAttack(clock))
+    }
+
+    private fun sineWithEarlyAttack(clock: Clock): Float {
+        return (((sin(beatWithinMeasure(clock) % 1f - .87) * 2 * PI) * 1.25 + 1) / 2.0).toFloat()
+    }
+
+    private fun sawtooth(clock: Clock): Float {
+        return 1 - beatWithinMeasure(clock) % 1.0f
+    }
 
     /** Returns 1.0 if we're on the start of the measure, 0.0 when we're furthest from the start of the measure,
      * and anywhere in between otherwise. */
     fun fractionTillNextMeasure(clock: Clock): Float =
         if (beatIntervalMs == 0) -1f else 1 - timeSinceMeasure(clock)
+
+    private fun clamp(f: Float): Float = min(1f, max(f, 0f))
 }
 
 
