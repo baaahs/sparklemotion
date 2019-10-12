0f8c8048f8beeb49e1c8e4363ed203091a0aead6
Christian Williams <antixian666@gmail.com>
2019-10-12 15:01:57 -0700

    Better SurfacePixelStratregy uses linear interpolation along arbitrary lines.

--- src/commonMain/kotlin/baaahs/shaders/SimpleSpatialShader.kt
+++ src/commonMain/kotlin/baaahs/shaders/SimpleSpatialShader.kt
@@ -1,7 +1,7 @@
 package baaahs.shaders
 
 import baaahs.*
-import baaahs.glsl.DefaultSurfacePixelStrategy
+import baaahs.glsl.LinearSurfacePixelStrategy
 import baaahs.glsl.PanelSpaceUvTranslator
 import baaahs.io.ByteArrayReader
 import baaahs.io.ByteArrayWriter
@@ -48,7 +48,7 @@ class SimpleSpatialShader() : Shader<SimpleSpatialShader.Buffer>(ShaderId.SIMPLE
     class Renderer(surface: Surface) : Shader.Renderer<Buffer> {
         private val uvTranslator =
             if (surface is IdentifiedSurface) {
-                PanelSpaceUvTranslator.forPixels(DefaultSurfacePixelStrategy.forSurface(surface))
+                PanelSpaceUvTranslator.forPixels(LinearSurfacePixelStrategy.forSurface(surface))
             } else null
 
         override fun draw(buffer: Buffer, pixelIndex: Int): Color {
