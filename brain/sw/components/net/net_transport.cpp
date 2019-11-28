d778a441cbd7c94703caa41d91961c2ccc81bc74
Christian Williams <antixian666@gmail.com>
2019-11-27 17:32:46 -0800

    Revert "Disabling wifi"
    This partially reverts commit ed4c3b18712958799b6650dd33052f0317827571.

--- brain/sw/components/net/net_transport.cpp
+++ brain/sw/components/net/net_transport.cpp
@@ -184,9 +184,9 @@ NetTransport::reconfigure() {
     m_pEth->setEnabled(true);
 #endif
 
-//    wifiSta();
-//    m_pWifiSta->setCredentials(GlobalConfig.staSsid(), GlobalConfig.staPass());
-//    m_pWifiSta->setEnabled(true);
+    wifiSta();
+    m_pWifiSta->setCredentials(GlobalConfig.staSsid(), GlobalConfig.staPass());
+    m_pWifiSta->setEnabled(true);
 //
 //    wifiAp();
 //    m_pWifiAp->setEnabled(true);
