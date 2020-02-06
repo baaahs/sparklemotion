0201a11ee37f93e42d441517b398ecce596ba920
Christian Williams <antixian666@gmail.com>
2020-02-05 23:09:58 -0800

    Mapper works even better.

--- src/commonMain/kotlin/baaahs/Mapper.kt
+++ src/commonMain/kotlin/baaahs/Mapper.kt
@@ -190,6 +190,7 @@ class Mapper(
                 brainsToMap.values.forEachIndexed { index, brainToMap ->
                     identifyBrain(index, brainToMap)
 
+                    // the next line causes the UI to wait after each panel has been identified...
                     pauseForUserInteraction()
 
                     var retryCount = 0
@@ -433,6 +434,11 @@ class Mapper(
                 }
             }
 
+            if (sampleLocations.isEmpty()) {
+                logger.warn { "Failed to match anything up with ${brainToMap.brainId}, bailing." }
+                return
+            }
+
             val surfaceBallot = Ballot<MapperUi.VisibleSurface>()
             var tries = 1000
             while (surfaceBallot.totalVotes < 10 && tries-- > 0) {
@@ -444,7 +450,13 @@ class Mapper(
                 }
             }
 
-            if (tries == 0) return
+            if (tries == 0 || surfaceBallot.noVotes()) {
+                logger.warn {
+                    "Failed to cast sufficient votes (${surfaceBallot.totalVotes}) after 1000 tries" +
+                        " on ${brainToMap.brainId}, bailing."
+                }
+                return
+            }
 
             //                val orderedPanels = visibleSurfaces.map { visiblePanel ->
             //                    visiblePanel to visiblePanel.boxOnScreen.distanceTo(surfaceChangeRegion)
@@ -631,7 +643,10 @@ class Mapper(
 
                 outstanding.values.removeAll {
                     if (it.failAt < nowMs) {
-                        logger.debug { "Timed out waiting for ${it.brainToMap.brainId} pong ${it.key.stringify()}" }
+                        logger.debug {
+                            "Timed out waiting after ${nowMs - it.sentAt}ms for ${it.brainToMap.brainId}" +
+                                " pong ${it.key.stringify()}"
+                        }
                         it.failed()
                         true
                     } else {
@@ -639,7 +654,7 @@ class Mapper(
 
                         if (it.retryAt < nowMs) {
                             logger.warn {
-                                "Haven't heard from ${it.brainToMap.brainId} after ${nowMs - it.retryAt}," +
+                                "Haven't heard from ${it.brainToMap.brainId} after ${nowMs - it.sentAt}ms," +
                                         " retrying (attempt ${++it.retryCount})..."
                             }
                             it.attemptDelivery()
@@ -796,6 +811,8 @@ class Mapper(
             box.getOrPut(key) { Vote(value) }.votes++
             totalVotes++
         }
+
+        fun noVotes(): Boolean = box.isEmpty()
 
         fun winner(): T {
             return box.values.sortedByDescending { it.votes }.first().item
