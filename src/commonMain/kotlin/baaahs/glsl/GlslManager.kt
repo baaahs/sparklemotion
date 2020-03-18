662aa504e093219f741ce54752a70cf8d58e0b69
Christian Williams <antixian666@gmail.com>
2020-03-18 11:03:03 -0700

    Prepare for large shader rewrite.
    Rename Shader to CompiledShader.
    Encapsulate Kgl better.

--- src/commonMain/kotlin/baaahs/glsl/GlslManager.kt
+++ src/commonMain/kotlin/baaahs/glsl/GlslManager.kt
@@ -1,5 +1,7 @@
 package baaahs.glsl
 
+import com.danielgergely.kgl.GL_FRAGMENT_SHADER
+import com.danielgergely.kgl.GL_VERTEX_SHADER
 import com.danielgergely.kgl.Kgl
 
 abstract class GlslManager(private val glslVersion: String) {
@@ -13,7 +15,7 @@ abstract class GlslManager(private val glslVersion: String) {
 
     fun createProgram(fragShader: String): Program {
         return runInContext {
-            Program(kgl, fragShader, glslVersion, GlslBase.plugins)
+            Program(this, fragShader, glslVersion, GlslBase.plugins)
         }
     }
 
@@ -26,5 +28,21 @@ abstract class GlslManager(private val glslVersion: String) {
                 override fun <T> inContext(fn: () -> T): T = runInContext(fn)
             }, program, uvTranslator)
         }
+    }
+
+    fun createVertexShader(source: String): CompiledShader {
+        val shaderId = check { createShader(GL_VERTEX_SHADER) } ?: throw IllegalStateException()
+        return CompiledShader(kgl, shaderId, source)
+    }
+
+    fun createFragmentShader(source: String): CompiledShader {
+        val shaderId = check { createShader(GL_FRAGMENT_SHADER) ?: throw IllegalStateException() }
+        return CompiledShader(kgl, shaderId, source)
+    }
+
+    fun <T> check(fn: Kgl.() -> T): T {
+        val result = kgl.fn()
+        kgl.checkForGlError()
+        return result
     }
 }
