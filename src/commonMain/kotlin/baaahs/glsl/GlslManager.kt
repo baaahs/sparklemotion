082353f23a634c6f35d457bce5d07e94c1443d5e
Christian Williams <antixian666@gmail.com>
2020-03-28 13:50:49 -0700

    All GLSL operations require an explicit GlslContext.

--- src/commonMain/kotlin/baaahs/glsl/GlslManager.kt
+++ src/commonMain/kotlin/baaahs/glsl/GlslManager.kt
@@ -1,30 +1,7 @@
 package baaahs.glsl
 
-import com.danielgergely.kgl.Kgl
-
-abstract class GlslManager(private val glslVersion: String) {
+abstract class GlslManager {
     abstract val available: Boolean
 
-    protected val kgl: Kgl by lazy { createContext() }
-
-    abstract fun createContext(): Kgl
-
-    abstract fun <T> runInContext(fn: () -> T): T
-
-    fun createProgram(fragShader: String): Program {
-        return runInContext {
-            Program(kgl, fragShader, glslVersion, GlslBase.plugins)
-        }
-    }
-
-    fun createRenderer(
-        program: Program,
-        uvTranslator: UvTranslator
-    ): GlslRenderer {
-        return runInContext {
-            GlslRenderer(kgl, object : GlslRenderer.ContextSwitcher {
-                override fun <T> inContext(fn: () -> T): T = runInContext(fn)
-            }, program, uvTranslator)
-        }
-    }
+    abstract fun createContext(): GlslContext
 }
