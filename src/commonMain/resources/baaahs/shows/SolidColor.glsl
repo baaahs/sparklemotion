c0eac6fc0cec56590c612fefc40bdce3c2009f42
Christian Williams <antixian666@gmail.com>
2020-05-02 21:54:49 -0700

    Less silly text sizes. Decent GLSL version of Solid Color, now with sparkles!

--- src/commonMain/resources/baaahs/shows/SolidColor.glsl
+++ src/commonMain/resources/baaahs/shows/SolidColor.glsl
@@ -1,7 +1,22 @@
-// Solid Colorz.glsl
+// Solid Color.glsl
 
-uniform vec4 color;
+uniform float time;
+uniform vec2 resolution;
+uniform vec4 color; // @@ColorPicker
+uniform float brightness;
+
+// @@Slider
+//   default=.02
+//   scale=log
+//   desc:
+//     How sparkly do you want it?
+uniform float sparkliness;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123 + time);
+}
 
 void main() {
-    gl_FragColor = color;
+    gl_FragColor = color * brightness
+        + ((sparkliness < random(gl_FragCoord.xy / resolution.xy)) ? vec4(0.) : vec4(1.));
 }
