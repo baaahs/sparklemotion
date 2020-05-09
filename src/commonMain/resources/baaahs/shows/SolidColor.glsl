70e21ec2e35352084f68c930f3f89ba637db00d2
Christian Williams <antixian666@gmail.com>
2020-05-08 19:23:23 -0700

    Nicer defaults for SolidColor shader.

--- src/commonMain/resources/baaahs/shows/SolidColor.glsl
+++ src/commonMain/resources/baaahs/shows/SolidColor.glsl
@@ -2,14 +2,9 @@
 
 uniform float time;
 uniform vec2 resolution;
-uniform vec4 color; // @@ColorPicker
-uniform float brightness;
-
-// @@Slider
-//   default=.02
-//   scale=log
-//   desc="How sparkly do you want it?"
-uniform float sparkliness;
+uniform vec4 color; // @@ColorPicker default=#3300ff
+uniform float brightness; // @@Slider default=.75
+uniform float sparkliness; // @@Slider default=.02 max=.5
 
 float random (vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123 + time);
