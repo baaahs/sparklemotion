dbc3c612478f55667a88061645f0780846797538
Christian Williams <antixian666@gmail.com>
2020-05-18 10:30:01 -0700

    Detect when cursor is over a number in shader editor.

--- src/jsMain/js/simulator/windows/ShaderEditorWindow/ShaderEditorWindow.jsx
+++ src/jsMain/js/simulator/windows/ShaderEditorWindow/ShaderEditorWindow.jsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useContext, useEffect, useRef, useState,} from 'react';
 import AceEditor from 'react-ace';
+import {Range} from 'ace-builds';
 import classNames from 'classnames';
 import styles from './ShaderEditorWindow.scss';
 import 'ace-builds/src-noconflict/mode-glsl';
@@ -20,6 +21,8 @@ const ShaderEditorWindow = (props) => {
   const [glslPreviewer, setGlslPreviewer] = useState(null);
   const [gadgets, setGadgets] = useState([]);
   const [openShaders, setOpenShaders] = useState([]);
+  const [extractionCandidate, setExtractionCandidate] = useState(null);
+  let glslNumberMarker = null;
 
   // Anytime the sheepView div is resized,
   // ask the Visualizer to resize the 3D sheep canvas
@@ -72,6 +75,40 @@ const ShaderEditorWindow = (props) => {
         updatePreview(newValue);
       } catch (e) {
         console.error("Uncaught exception in editor onChange", e);
+      }
+    },
+    [setShowStr, glslPreviewer]
+  );
+
+  const glslNumberRegex = /[0-9.]/;
+  const glslIllegalRegex = /[A-Za-z_]/;
+  const glslFloatRegex = /([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)/;
+  const onCursorChange = useCallback(
+    (selection) => {
+      const session = selection.session;
+      if (glslNumberMarker) {
+        session.removeMarker(glslNumberMarker);
+      }
+
+      const cursor = selection.getCursor();
+      const line = session.getDocument().getLine(cursor.row);
+      let start = cursor.column;
+      let end = cursor.column;
+      while (glslNumberRegex.test(line.charAt(start - 1))) start--;
+      while (glslNumberRegex.test(line.charAt(end))) end++;
+      let badCharBefore = start > 0 && glslIllegalRegex.test(line.charAt(start - 1));
+      let badCharAfter = end < line.length - 1 && glslIllegalRegex.test(line.charAt(end));
+      const candidate = line.substring(start, end);
+      let looksLikeFloatOrInt = glslFloatRegex.test(candidate);
+      if (badCharBefore || badCharAfter || !looksLikeFloatOrInt) {
+        if (extractionCandidate?.text !== null) setExtractionCandidate({text: null, range: null});
+      } else {
+        const range = new Range(cursor.row, start, cursor.row, end);
+        glslNumberMarker = session.addMarker(range, styles.glslNumber, "text", false);
+
+        if (extractionCandidate?.text !== candidate) {
+          setExtractionCandidate({text: candidate, range: range});
+        }
       }
     },
     [setShowStr, glslPreviewer]
@@ -132,10 +169,12 @@ const ShaderEditorWindow = (props) => {
         showGutter={true}
         setAutoScrollEditorIntoView={true}
         onChange={onChange}
+        onCursorChange={onCursorChange}
         value={showStr}
         name="ShaderEditorWindow"
         editorProps={{ $blockScrolling: true }}
       />
+      <div>Extract {extractionCandidate?.text}?</div>
     </div>
   );
 };
