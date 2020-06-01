5c3d1d91203869b83d452e9ae63c03b388db2099
Christian Williams <antixian666@gmail.com>
2020-06-01 13:28:56 -0700

    Use new useState.

--- src/jsMain/kotlin/baaahs/ui/util.kt
+++ src/jsMain/kotlin/baaahs/ui/util.kt
@@ -3,10 +3,7 @@ package baaahs.ui
 import react.RMutableRef
 
 @Suppress("UNCHECKED_CAST")
-fun <T> noSuch(): T = null as T
+fun <T> nuffin(): T = null as T
 
 @Suppress("UNCHECKED_CAST")
 fun <T> useRef(): RMutableRef<T> = react.useRef(null as T)
-
-@Suppress("UNCHECKED_CAST")
-fun <T> useState() = react.useState(null as T)
