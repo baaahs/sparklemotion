2b540f3342788c5ae3a3db3afa61f2855a1dee7f
Christian Williams <antixian666@gmail.com>
2020-07-04 18:47:40 -0700

    Extract dependency versions to buildSrc.

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+    val kotlin = "1.3.72"
+    val coroutines = "1.3.6"
+    val serialization = kotlin
+    val serialization_runtime = "0.20.0"
+    val ktor = "1.3.2"
+    val kgl = "0.3-baaahs"
+    val jogl = "2.3.2"
+    val lwjgl = "3.2.3"
+    val spek = "2.0.11-rc.1" // custom build with LetValues enabled
+    val atrium = "0.10.0"
+}
