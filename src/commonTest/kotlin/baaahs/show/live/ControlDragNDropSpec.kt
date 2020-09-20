51a985ec7c030441ddb76fc7e6d59f46305c63fb
Christian Williams <antixian666@gmail.com>
2020-09-20 11:57:33 -0700

    Refactor drag & drop tests a little.

--- src/commonTest/kotlin/baaahs/show/live/ControlDragNDropSpec.kt
+++ src/commonTest/kotlin/baaahs/show/live/ControlDragNDropSpec.kt
@@ -1,14 +1,16 @@
 package baaahs.show.live
 
+import baaahs.app.ui.Draggable
 import baaahs.getBang
-import baaahs.gl.patch.AutoWirer
-import baaahs.plugin.Plugins
-import baaahs.show.ButtonControl
+import baaahs.gl.override
+import baaahs.show.live.ControlDisplay.PanelBuckets.PanelBucket
 import baaahs.show.mutable.MutableShow
 import baaahs.show.mutable.ShowBuilder
 import baaahs.shows.FakeGlContext
 import baaahs.shows.FakeKgl
 import baaahs.shows.FakeShowPlayer
+import baaahs.toBeSpecified
+import baaahs.unknown
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.expect
@@ -22,14 +24,16 @@ object ControlDragNDropSpec : Spek({
         val show by value { mutableShow.build(ShowBuilder()) }
 
         val showPlayer by value { FakeShowPlayer(FakeGlContext(FakeKgl())) }
-        val autoWirer by value { AutoWirer(Plugins.safe()) }
-        val showOpener by value { ShowOpener(autoWirer.glslAnalyzer, show, showPlayer) }
-        val openShow by value { showOpener.openShow() }
+        val openShow by value { show.open(showPlayer) }
         val editMode by value { true }
         val editHandler by value { FakeEditHandler() }
         val dragNDrop by value { FakeDragNDrop() }
-        val controlDisplay by value {
-            ControlDisplay(openShow, editMode, editHandler, dragNDrop)
+        val controlDisplay by value { ControlDisplay(openShow, editMode, editHandler, dragNDrop) }
+
+        fun renderEditedShow(): String {
+            val editedOpenShow = editHandler.updatedShow.open(showPlayer)
+            val newControlDisplay = ControlDisplay(editedOpenShow, editMode, editHandler, dragNDrop)
+            return editedOpenShow.fakeRender(newControlDisplay)
         }
 
         beforeEachTest {
@@ -38,34 +42,54 @@ object ControlDragNDropSpec : Spek({
             mutableShow.addButton("Panel 1", "Button B") {}
         }
 
-        val panel1Buckets by value {
-            val buckets = arrayListOf<ControlDisplay.PanelBuckets.PanelBucket>()
-            controlDisplay.render("Panel 1") { panelBucket -> buckets.add(panelBucket) }
-            buckets
+        val panelBuckets by value {
+            openShow.layouts.panelNames.associateWith { panelName ->
+                controlDisplay.renderBuckets(panelName)
+            }
         }
-        val panel1ShowBucket by value { panel1Buckets.first() }
-        val panel1SceneBucket by value { panel1Buckets[1] }
 
-        it("has the expected initial state") {
+        fun findBucket(panelName: String, sectionTitle: String): PanelBucket {
+            val panelBucket = panelBuckets.getBang(panelName, "Panel")
+            return panelBucket.find { it.section.title == sectionTitle }
+                ?: error(unknown("section", sectionTitle, panelBucket.map { it.section.title }))
+        }
+
+        val fromBucket by value { toBeSpecified<PanelBucket>() }
+        val toBucket by value { toBeSpecified<PanelBucket>() }
+        val draggedControl by value { toBeSpecified<Draggable>() }
+
+        it("has the expected initial appearance") {
             expect(
-                listOf("scenesButtonGroup", "buttonAButton", "buttonBButton")
-            ) { panel1ShowBucket.controls.map { it.control.id } }
+                """
+                    Panel 1:
+                      |Show| scenesButtonGroup[*scene1Button*, scene2Button], buttonAButton, buttonBButton
+                      |Scene 1|
+                      |Backdrop 1.1|
+                    Panel 2:
+                      |Show|
+                      |Scene 1| backdropsButtonGroup[*backdrop11Button*, backdrop12Button]
+                      |Backdrop 1.1|
+                    Panel 3:
+                      |Show|
+                      |Scene 1| slider1SliderControl
+                      |Backdrop 1.1|
+                """.trimIndent()
+            ) { openShow.fakeRender(controlDisplay) }
         }
 
-        context("when a control is dragged") {
-            val buttonADraggable by value { panel1ShowBucket.getDraggable(1) }
+        context("when a button is dragged") {
+            override(fromBucket) { findBucket("Panel 1", "Show") }
+            override(draggedControl) { fromBucket.getDraggable(1) }
 
             it("can be dropped back in the same bucket") {
-                expect(true) { buttonADraggable.willMoveTo(panel1ShowBucket) }
-            }
-
-            it("can be dropped back into another bucket") {
-                expect(true) { buttonADraggable.willMoveTo(panel1SceneBucket) }
+                expect(true) { draggedControl.willMoveTo(fromBucket) }
             }
 
             context("and dropped within the same DropTarget") {
+                override(toBucket) { fromBucket }
+
                 beforeEachTest {
-                    dragNDrop.doMove(panel1ShowBucket, 1, panel1ShowBucket, 2)
+                    dragNDrop.doMove(fromBucket, 1, fromBucket, 2)
                 }
 
                 it("reorders controls in the parent") {
@@ -75,25 +99,36 @@ object ControlDragNDropSpec : Spek({
                 }
             }
 
+            it("can be dropped back into another bucket") {
+                expect(true) { draggedControl.willMoveTo(findBucket("Panel 1", "Scene 1")) }
+            }
+
             context("and dropped to another DropTarget") {
+                override(toBucket) { findBucket("Panel 1", "Scene 1") }
+
                 beforeEachTest {
-                    dragNDrop.doMove(panel1ShowBucket, 1, panel1SceneBucket, 0)
+                    dragNDrop.doMove(fromBucket, 1, toBucket, 0)
                 }
 
-                it("removes the control from prior parent") {
+                it("removes the control from prior parent and adds it to the new parent") {
                     expect(
-                        listOf("scenesButtonGroup", "buttonBButton")
-                    ) { editHandler.updatedShow.controlLayout["Panel 1"] }
-                }
-
-                it("adds the control from its new parent") {
-                    val scene1ButtonAfterEdit =
-                        editHandler.updatedShow.controls.getBang("scene1Button", "Control") as ButtonControl
-
-                    expect(listOf("buttonAButton")) { scene1ButtonAfterEdit.controlLayout["Panel 1"] }
+                        """
+                            Panel 1:
+                              |Show| scenesButtonGroup[*scene1Button*, scene2Button], buttonBButton
+                              |Scene 1| buttonAButton
+                              |Backdrop 1.1|
+                            Panel 2:
+                              |Show|
+                              |Scene 1| backdropsButtonGroup[*backdrop11Button*, backdrop12Button]
+                              |Backdrop 1.1|
+                            Panel 3:
+                              |Show|
+                              |Scene 1| slider1SliderControl
+                              |Backdrop 1.1|
+                        """.trimIndent()
+                    ) { renderEditedShow() }
                 }
             }
         }
     }
-
 })
