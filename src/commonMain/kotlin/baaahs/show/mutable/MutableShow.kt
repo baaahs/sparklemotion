c555c3a7ab64e0f15386b01e3597f66ff8566c54
Christian Williams <antixian666@gmail.com>
2020-10-20 16:54:41 -0700

    Move MutableControls out of MutableShow.

--- src/commonMain/kotlin/baaahs/show/mutable/MutableShow.kt
+++ src/commonMain/kotlin/baaahs/show/mutable/MutableShow.kt
@@ -1,13 +1,15 @@
 package baaahs.show.mutable
 
-import baaahs.*
+import baaahs.ShowState
 import baaahs.app.ui.EditorPanel
 import baaahs.app.ui.MutableEditable
 import baaahs.app.ui.editor.*
+import baaahs.getBang
 import baaahs.gl.patch.AutoWirer
 import baaahs.gl.patch.ContentType
 import baaahs.gl.patch.LinkedPatch
 import baaahs.gl.shader.OpenShader
+import baaahs.randomId
 import baaahs.show.*
 import baaahs.show.live.*
 import baaahs.util.CacheBuilder
@@ -353,103 +355,6 @@ class MutablePatch {
 
 fun DataSource.editor() = MutableDataSourcePort(this)
 
-interface MutableControl : MutableEditable {
-    var asBuiltId: String?
-    fun build(showBuilder: ShowBuilder): Control
-    fun accept(visitor: MutableShowVisitor, log: VisitationLog) {
-        if (log.controls.add(this)) visitor.visit(this)
-    }
-}
-
-class MutableButtonControl(
-    baseButtonControl: ButtonControl,
-    override val mutableShow: MutableShow
-) : MutablePatchHolder(baseButtonControl), MutableControl {
-    var activationType: ButtonControl.ActivationType = baseButtonControl.activationType
-
-    override var asBuiltId: String? = null
-
-    override fun getPropertiesComponents(): List<PropsEditor> {
-        return super.getPropertiesComponents() + ButtonPropsEditor(this)
-    }
-
-    override fun build(showBuilder: ShowBuilder): Control {
-        return ButtonControl(
-            title,
-            activationType,
-            patches = patches.map { it.build(showBuilder) },
-            eventBindings = eventBindings,
-            controlLayout = buildControlLayout(showBuilder)
-        )
-    }
-
-    override fun accept(visitor: MutableShowVisitor, log: VisitationLog) {
-        super<MutablePatchHolder>.accept(visitor, log)
-        super<MutableControl>.accept(visitor, log)
-    }
-}
-
-data class MutableButtonGroupControl(
-    override var title: String,
-    var direction: ButtonGroupControl.Direction,
-    val buttons: MutableList<MutableButtonControl> = arrayListOf(),
-    val mutableShow: MutableShow
-) : MutableControl {
-    override var asBuiltId: String? = null
-
-    fun addButton(title: String, block: MutableButtonControl.() -> Unit): MutableButtonControl {
-        val control = MutableButtonControl(ButtonControl(title), mutableShow)
-        control.block()
-        buttons.add(control)
-        return control
-    }
-
-    override fun getEditorPanels(): List<EditorPanel> {
-        return listOf(
-            GenericPropertiesEditorPanel(
-                ButtonGroupPropsEditor(this)
-            )
-        )
-    }
-
-    override fun build(showBuilder: ShowBuilder): Control {
-        return ButtonGroupControl(title, direction, buttons.map { mutableButtonControl ->
-            mutableButtonControl.buildAndStashId(showBuilder)
-        })
-    }
-
-    override fun accept(visitor: MutableShowVisitor, log: VisitationLog) {
-        super.accept(visitor, log)
-        buttons.forEach { it.accept(visitor, log) }
-    }
-
-    fun moveButton(fromIndex: Int, toIndex: Int) {
-        buttons.add(toIndex, buttons.removeAt(fromIndex))
-    }
-}
-
-data class MutableGadgetControl(
-    var gadget: Gadget,
-    val controlledDataSource: DataSource
-) : MutableControl {
-    override val title: String
-        get() = gadget.title
-
-    override var asBuiltId: String? = null
-
-    override fun getEditorPanels(): List<EditorPanel> {
-        return emptyList()
-    }
-
-    override fun build(showBuilder: ShowBuilder): Control {
-        return GadgetControl(gadget, showBuilder.idFor(controlledDataSource))
-    }
-
-    fun open(): OpenControl {
-        return OpenGadgetControl(randomId(gadget.title.camelize()), gadget, controlledDataSource)
-    }
-}
-
 data class MutableShader(
     var title: String,
     var type: ShaderType,
@@ -526,12 +431,6 @@ data class MutableShaderInstance(
             { it.mutableShader.title }
         )
     }
-}
-
-
-private fun MutableControl.buildAndStashId(showBuilder: ShowBuilder): String {
-    return showBuilder.idFor(build(showBuilder))
-        .also { asBuiltId = it }
 }
 
 
