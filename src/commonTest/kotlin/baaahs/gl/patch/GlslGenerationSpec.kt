bc1210f48b35669b65274f7c2facc04e2eb2a4bb
Christian Williams <antixian666@gmail.com>
2020-10-20 16:21:44 -0700

    Add spec of GLSL generation for multi-channel shader patches. Add log warning when there's missing components on a channel.

--- src/commonTest/kotlin/baaahs/gl/patch/GlslGenerationSpec.kt
+++ src/commonTest/kotlin/baaahs/gl/patch/GlslGenerationSpec.kt
@@ -37,14 +37,14 @@ object GlslGenerationSpec : Spek({
         }
         val autoWirer by value { AutoWirer(Plugins.safe()) }
         val glslAnalyzer by value { autoWirer.glslAnalyzer }
-        val paintShader by value { glslAnalyzer.import(shaderText) }
+        val mainShader by value { glslAnalyzer.import(shaderText) }
         val mutablePatch by value { MutablePatch { } }
         val linkedPatch by value { mutablePatch.openForPreview(autoWirer)!! }
         val glsl by value { linkedPatch.toGlsl().trim() }
 
         context("with screen coordinates for preview") {
             beforeEachTest {
-                mutablePatch.addShaderInstance(paintShader) {
+                mutablePatch.addShaderInstance(mainShader) {
                     link("gl_FragCoord", CorePlugin.ScreenUvCoordDataSource())
                     link("resolution", CorePlugin.ResolutionDataSource())
                     link("time", CorePlugin.TimeDataSource())
@@ -125,7 +125,7 @@ object GlslGenerationSpec : Spek({
             }
 
             beforeEachTest {
-                mutablePatch.addShaderInstance(paintShader) {
+                mutablePatch.addShaderInstance(mainShader) {
                     link(
                         "blueness",
                         CorePlugin.SliderDataSource("Blueness", 0f, 0f, 1f, null)
@@ -202,7 +202,7 @@ object GlslGenerationSpec : Spek({
                         shaderChannel = MutableShaderChannel(ShaderChannel.Main.id)
                     }
 
-                    addShaderInstance(paintShader) {
+                    addShaderInstance(mainShader) {
                         link(
                             "gl_FragCoord",
                             MutableShaderOutPort(
@@ -306,6 +306,161 @@ object GlslGenerationSpec : Spek({
                         }
                     """.trimIndent()
                 ) { glsl }
+            }
+        }
+
+        describe("mixing from another channel") {
+            val otherShaderActualChannel by value { "other" }
+
+            override(shaderText) {
+                """
+                    // Cross-fade shader
+                    varying vec4 inColor2; // @type color-stream
+                    uniform float fade;
+
+                    vec4 mainFilter(vec4 inColor) {
+                        return mix(inColor, inColor2, fade);
+                    }
+                """.trimIndent()
+            }
+
+            val mainPaintShader by value {
+                glslAnalyzer.import(
+                    """
+                    void main( void ) {
+                        gl_FragColor = vec4(1., 0., 0., 1.);
+                    }
+                """.trimIndent(), "Main Paint Shader"
+                )
+            }
+
+            val otherPaintShader by value {
+                glslAnalyzer.import(
+                    """
+                    void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+                        fragColor = vec4(0., 1., 0., 1.);
+                    }
+                """.trimIndent(), "Other Paint Shader"
+                )
+            }
+
+            beforeEachTest {
+                mutablePatch.addShaderInstance(mainPaintShader)
+                mutablePatch.addShaderInstance(otherPaintShader) {
+                    shaderChannel = MutableShaderChannel.from(otherShaderActualChannel)
+                }
+
+                mutablePatch.addShaderInstance(mainShader) {
+                    link("fade", CorePlugin.SliderDataSource("Fade", 0f, 0f, 1f, null))
+                    link("gl_FragColor", MutableShaderChannel("main"))
+                    link("inColor2", MutableShaderChannel("other"))
+                }
+            }
+
+            it("generates GLSL") {
+                expect(
+                    /**language=glsl*/
+                    """
+                        #ifdef GL_ES
+                        precision mediump float;
+                        #endif
+
+                        // SparkleMotion-generated GLSL
+
+                        layout(location = 0) out vec4 sm_result;
+
+                        uniform float in_fadeSlider;
+
+                        // Shader: Main Paint Shader; namespace: p0
+                        // Main Paint Shader
+
+                        vec4 p0_mainPaintShader_gl_FragColor = vec4(0., 0., 0., 1.);
+
+                        #line 1
+                        void p0_mainPaintShader_main( void ) {
+                            p0_mainPaintShader_gl_FragColor = vec4(1., 0., 0., 1.);
+                        }
+
+                        // Shader: Other Paint Shader; namespace: p1
+                        // Other Paint Shader
+
+                        vec4 p1_otherPaintShaderi_result = vec4(0., 0., 0., 1.);
+
+                        #line 1
+                        void p1_otherPaintShader_mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+                            fragColor = vec4(0., 1., 0., 1.);
+                        }
+
+                        // Shader: Cross-fade shader; namespace: p2
+                        // Cross-fade shader
+
+                        vec4 p2_crossFadeShaderi_result = vec4(0., 0., 0., 1.);
+
+                        #line 5
+                        vec4 p2_crossFadeShader_mainFilter(vec4 inColor) {
+                            return mix(inColor, p1_otherPaintShaderi_result, in_fadeSlider);
+                        }
+
+
+                        #line 10001
+                        void main() {
+                          p0_mainPaintShader_main(); // Main Paint Shader
+                          p1_otherPaintShader_mainImage(p1_otherPaintShaderi_result, sm_FragCoord.xy); // Other Paint Shader
+                          p2_crossFadeShaderi_result = p2_crossFadeShader_mainFilter(p0_mainPaintShader_gl_FragColor); // Cross-fade shader
+                          sm_result = p2_crossFadeShaderi_result;
+                        }
+                    """.trimIndent()
+                ) { glsl }
+            }
+
+            context("when there's no paint shader on the other channel") {
+                override(otherShaderActualChannel) { "notOther" }
+
+                it("should report an error to the user BUT IT CURRENTLY DOESNT") {
+                    // TODO: WRONG WRONG WRONG we should tell the user that something's wrong?
+                    expect(
+                        /**language=glsl*/
+                        """
+                            #ifdef GL_ES
+                            precision mediump float;
+                            #endif
+
+                            // SparkleMotion-generated GLSL
+
+                            layout(location = 0) out vec4 sm_result;
+
+                            uniform float in_fadeSlider;
+
+                            // Shader: Main Paint Shader; namespace: p0
+                            // Main Paint Shader
+
+                            vec4 p0_mainPaintShader_gl_FragColor = vec4(0., 0., 0., 1.);
+
+                            #line 1
+                            void p0_mainPaintShader_main( void ) {
+                                p0_mainPaintShader_gl_FragColor = vec4(1., 0., 0., 1.);
+                            }
+
+                            // Shader: Cross-fade shader; namespace: p1
+                            // Cross-fade shader
+
+                            vec4 p1_crossFadeShaderi_result = vec4(0., 0., 0., 1.);
+
+                            #line 5
+                            vec4 p1_crossFadeShader_mainFilter(vec4 inColor) {
+                                return mix(inColor, p1_crossFadeShader_inColor2, in_fadeSlider);
+                            }
+
+
+                            #line 10001
+                            void main() {
+                              p0_mainPaintShader_main(); // Main Paint Shader
+                              p1_crossFadeShaderi_result = p1_crossFadeShader_mainFilter(p0_mainPaintShader_gl_FragColor); // Cross-fade shader
+                              sm_result = p1_crossFadeShaderi_result;
+                            }
+                        """.trimIndent()
+                    ) { glsl }
+                }
             }
         }
     }
