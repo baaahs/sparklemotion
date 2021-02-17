63588b8f8b658e2ff3d16b4fb5c11c7fbc4865f8
Christian Williams <antixian666@gmail.com>
2021-02-17 09:35:31 -0800

    Create migration from old layouts format to new.
    Since probably nobody's created a custom layout yet, we'll just ignore what's there and drop in a pre-baked default.

--- src/commonMain/kotlin/baaahs/show/migration/V2_RemoveShaderType.kt
+++ src/commonMain/kotlin/baaahs/show/migration/V2_RemoveShaderType.kt
@@ -1,15 +1,17 @@
 package baaahs.show.migration
 
 import baaahs.show.ShowMigrator
-import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonPrimitive
 
 @Suppress("ClassName")
 object V2_RemoveShaderType : ShowMigrator.Migration(2) {
-    override fun apply(newJson: MutableMap<String, JsonElement>) {
-        newJson.mapObjsInDict("shaders") { _, shader ->
-            shader.remove("type")?.jsonPrimitive?.contentOrNull
-        }
+    override fun migrate(from: JsonObject): JsonObject {
+        return from.toMutableMap().apply {
+            mapObjsInDict("shaders") { _, shader ->
+                shader.remove("type")?.jsonPrimitive?.contentOrNull
+            }
+        }.toJsonObj()
     }
 }
--- src/commonTest/kotlin/baaahs/TestUtil.kt
+++ src/commonTest/kotlin/baaahs/TestUtil.kt
@@ -22,11 +22,15 @@ import ch.tutteli.atrium.api.fluent.en_GB.containsExactly
 import ch.tutteli.atrium.api.fluent.en_GB.isEmpty
 import ch.tutteli.atrium.api.verbs.expect
 import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.reporting.ReporterBuilder
 import ch.tutteli.atrium.logic._logicAppend
 import ch.tutteli.atrium.logic.notToBe
 import ch.tutteli.atrium.logic.toBe
+import ch.tutteli.atrium.reporting.Reporter
+import ch.tutteli.atrium.reporting.ReporterFactory
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import org.spekframework.spek2.dsl.GroupBody
 import org.spekframework.spek2.dsl.Skip
 import org.spekframework.spek2.meta.*
@@ -164,3 +168,35 @@ fun expectEmptyMap(block: () -> Map<*, *>) {
 
 fun <T> Expect<T>.toEqual(expected: T): Expect<T> = _logicAppend { toBe(expected) }
 fun <T> Expect<T>.toNotEqual(expected: T): Expect<T> = _logicAppend { notToBe(expected) }
+
+fun Expect<JsonObject>.toEqualJson(expected: String): Expect<JsonObject> =
+    _logicAppend { toBe(Json {}.parseToJsonElement(expected) as JsonObject) }
+
+fun useBetterSpekReporter() = BetterSpekReporterFactory()
+class BetterSpekReporterFactory : ReporterFactory {
+    init {
+        // we specify the factory here because we only need to specify it once and
+        // we do not want to specify it if it is not used. The verbs have to be loaded on their first usage
+        // and thus this is a good place.
+        ReporterFactory.specifyFactoryIfNotYetSet(ID)
+    }
+
+    override val id: String = ID
+
+    override fun create(): Reporter {
+        return ReporterBuilder.create()
+            .withoutTranslationsUseDefaultLocale()
+            .withDetailedObjectFormatter()
+            .withDefaultAssertionFormatterController()
+            .withDefaultAssertionFormatterFacade()
+            .withTextNextLineAssertionPairFormatter()
+            .withTextCapabilities()
+            .withNoOpAtriumErrorAdjuster()
+            .withOnlyFailureReporter()
+            .build()
+    }
+
+    companion object {
+        const val ID: String = "baaahs.default"
+    }
+}
