6bb72b05b757f1024fe6eb8b531f4875976b7629
Christian Williams <antixian666@gmail.com>
2021-03-13 12:43:24 -0800

    Use Dispatchers.IO coroutine context for bocking file operations in RealFs.

--- src/jvmMain/kotlin/baaahs/io/RealFs.kt
+++ src/jvmMain/kotlin/baaahs/io/RealFs.kt
@@ -1,5 +1,7 @@
 package baaahs.io
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
@@ -9,47 +11,52 @@ class RealFs(
     override val name: String,
     private val basePath: Path
 ) : Fs {
-    override suspend fun listFiles(directory: Fs.File): List<Fs.File> {
-        val dir = resolve(directory)
-        return if (Files.isDirectory(dir)) {
-            Files.list(dir).map { directory.resolve(it.fileName.toString()) }.toList()
-        } else {
-            emptyList()
+    override suspend fun listFiles(directory: Fs.File): List<Fs.File> =
+        withContext(Dispatchers.IO) {
+            val dir = resolve(directory)
+            if (Files.isDirectory(dir)) {
+                Files.list(dir).map { directory.resolve(it.fileName.toString()) }.toList()
+            } else {
+                emptyList()
+            }
         }
-    }
 
-    override suspend fun loadFile(file: Fs.File): String? {
-        val destPath = resolve(file)
-        try {
-            return Files.readAllBytes(destPath).decodeToString()
-        } catch (e: java.nio.file.NoSuchFileException) {
-            return null
+    override suspend fun loadFile(file: Fs.File): String? =
+        withContext(Dispatchers.IO) {
+            val destPath = resolve(file)
+            try {
+                Files.readAllBytes(destPath).decodeToString()
+            } catch (e: java.nio.file.NoSuchFileException) {
+                null
+            }
         }
-    }
 
-    override suspend fun saveFile(file: Fs.File, content: ByteArray, allowOverwrite: Boolean) {
-        val destPath = resolve(file)
-        if (!Files.exists(destPath.parent)) {
-            Files.createDirectories(destPath.parent)
+    override suspend fun saveFile(file: Fs.File, content: ByteArray, allowOverwrite: Boolean): Unit =
+        withContext(Dispatchers.IO)  {
+            val destPath = resolve(file)
+            if (!Files.exists(destPath.parent)) {
+                Files.createDirectories(destPath.parent)
+            }
+            Files.write(
+                destPath,
+                content,
+                if (allowOverwrite) StandardOpenOption.CREATE else StandardOpenOption.CREATE_NEW
+            )
         }
-        Files.write(
-            destPath,
-            content,
-            if (allowOverwrite) StandardOpenOption.CREATE else StandardOpenOption.CREATE_NEW
-        )
-    }
 
     override suspend fun saveFile(file: Fs.File, content: String, allowOverwrite: Boolean) {
         saveFile(file, content.encodeToByteArray(), allowOverwrite)
     }
 
-    override suspend fun exists(file: Fs.File): Boolean {
-        return Files.exists(resolve(file))
-    }
+    override suspend fun exists(file: Fs.File): Boolean =
+        withContext(Dispatchers.IO) {
+            Files.exists(resolve(file))
+        }
 
-    override suspend fun isDirectory(file: Fs.File): Boolean {
-        return Files.isDirectory(resolve(file))
-    }
+    override suspend fun isDirectory(file: Fs.File): Boolean =
+        withContext(Dispatchers.IO) {
+            Files.isDirectory(resolve(file))
+        }
 
     private fun resolve(file: Fs.File): Path {
         return basePath.resolve(file.fullPath)
