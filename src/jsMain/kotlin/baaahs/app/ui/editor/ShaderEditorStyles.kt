19b238c12b423aac113bbde5f3eb3ba71dd30626
Christian Williams <antixian666@gmail.com>
2021-04-23 13:01:46 -0700

    Improvements to shader preview.
    * Moved "Adjust Gadgets" to popup menu (now "Auto-Adjust Gadgets")".
    * Gadget adjustment now defaults to random small incremental changes rather than cycling through the full range of each slider individually.
    * "Full Range" switches to the old mode.
    * Gadget adjustments now apply to any control whose gadget implements Gadget#adjustALittleBit().
    * Added "Reset to Defaults" to menu.
    * Added "Show Diagnostics…" to menu.

--- src/jsMain/kotlin/baaahs/app/ui/editor/ShaderEditorStyles.kt
+++ src/jsMain/kotlin/baaahs/app/ui/editor/ShaderEditorStyles.kt
@@ -86,6 +86,20 @@ class ShaderEditorStyles(private val theme: MuiTheme) : StyleSheet("app-ui-edito
         important(::textAlign, TextAlign.right)
     }
 
+    val previewContainer by css {
+        position = Position.relative
+    }
+
+    val settingsMenuAffordance by css {
+        position = Position.absolute
+        padding(2.px)
+        backgroundColor = Color.white.withAlpha(.5)
+        width = 2.em
+        height = 2.em
+        right = .5.em
+        bottom = .5.em
+    }
+
     val showAdvancedMenuItem by css {
         paddingTop = 0.px
         paddingBottom = 0.px
