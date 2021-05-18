e3492a1097515d4dca279e1763a423d5cf45ecae
Christian Williams <antixian666@gmail.com>
2021-05-18 10:19:02 -0700

    Implement Vacuity control.
    Any relevant controls which aren't currently visible will be automatically shown in a vacuity.

--- src/commonMain/kotlin/baaahs/show/live/OpenControl.kt
+++ src/commonMain/kotlin/baaahs/show/live/OpenControl.kt
@@ -35,6 +35,7 @@ interface ControlViews {
     fun forColorPicker(openColorPickerControl: OpenColorPickerControl, controlProps: ControlProps): View
     fun forSlider(openSlider: OpenSliderControl, controlProps: ControlProps): View
     fun forTransition(openTransitionControl: OpenTransitionControl, controlProps: ControlProps): View
+    fun forVacuity(openVacuityControl: OpenVacuityControl, controlProps: ControlProps): View
     fun forVisualizer(openVisualizerControl: OpenVisualizerControl, controlProps: ControlProps): View
 }
 
