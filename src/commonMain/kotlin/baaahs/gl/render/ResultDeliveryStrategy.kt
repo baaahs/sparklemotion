0b0c489290eee912d6b9dc4a574006bde7a91633
Christian Williams <antixian666@gmail.com>
2021-07-19 07:22:21 -0700

    Fix MovingHeadParams.contentType being null due to static initialization order. "0" == 0.px.

--- src/commonMain/kotlin/baaahs/gl/render/ResultDeliveryStrategy.kt
+++ src/commonMain/kotlin/baaahs/gl/render/ResultDeliveryStrategy.kt
@@ -2,7 +2,6 @@ package baaahs.gl.render
 
 import baaahs.fixtures.ResultBuffer
 import baaahs.gl.GlContext
-import baaahs.time
 
 interface ResultDeliveryStrategy {
     fun beforeRender() {}
@@ -20,20 +19,17 @@ interface ResultDeliveryStrategy {
 
 class SyncResultDeliveryStrategy : ResultDeliveryStrategy {
     override suspend fun awaitResults(frameBuffer: GlContext.FrameBuffer, resultBuffers: List<ResultBuffer>) {
-        val ms = time {
-            resultBuffers.forEach {
-                val gpuBuffer = it.gpuBuffer
-                val resultType = it.type
+        resultBuffers.forEach {
+            val gpuBuffer = it.gpuBuffer
+            val resultType = it.type
 
-                frameBuffer.withRenderBufferAsAttachment0(gpuBuffer) {
-                    gpuBuffer.readPixels(
-                        0, 0, gpuBuffer.curWidth, gpuBuffer.curHeight,
-                        resultType.readPixelFormat, resultType.readType, it.cpuBuffer
-                    )
-                }
+            frameBuffer.withRenderBufferAsAttachment0(gpuBuffer) {
+                gpuBuffer.readPixels(
+                    0, 0, gpuBuffer.curWidth, gpuBuffer.curHeight,
+                    resultType.readPixelFormat, resultType.readType, it.cpuBuffer
+                )
             }
         }
-        println("sync.awaitResults() took ${ms}ms")
     }
 }
 
