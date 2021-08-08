8e7617d5a7d82438d169622bfc4e4301f7f6ca91
Christian Williams <antixian666@gmail.com>
2021-08-08 09:23:00 -0700

    Models may generate their own mapping results.

--- src/commonMain/kotlin/baaahs/mapper/MappingResults.kt
+++ src/commonMain/kotlin/baaahs/mapper/MappingResults.kt
@@ -15,36 +15,45 @@ class SessionMappingResults(model: Model, mappingSessions: List<MappingSession>)
 
     init {
         mappingSessions.forEach { mappingSession ->
-            mappingSession.surfaces.forEach { surfaceData ->
-                val controllerId = surfaceData.controllerId
-                val entityName = surfaceData.entityName
+            mappingSession.surfaces.forEach { entityData ->
+                val controllerId = entityData.controllerId
+                val entityName = entityData.entityName
 
                 try {
                     val modelEntity = model.getEntity(entityName)
                     if (modelEntity == null)
                         logger.warn { "Unknown model entity \"$entityName\"." }
 
-                    val pixelLocations = surfaceData.pixels?.map { it?.modelPosition }
+                    val pixelLocations = entityData.pixels?.map { it?.modelPosition }
                         ?.ifEmpty { null }
-                    val pixelCount = surfaceData.pixelCount ?: pixelLocations?.size
+                    val pixelCount = entityData.pixelCount ?: pixelLocations?.size
                     val transportConfig = when (controllerId.controllerType) {
-                        "SACN" -> surfaceData.channels?.let { SacnTransportConfig(it.start, it.end) }
-                        "DMX" -> surfaceData.channels?.let { DmxTransportConfig(it.start, it.end) }
+                        "SACN" -> entityData.channels?.let { SacnTransportConfig(it.start, it.end) }
+                        "DMX" -> entityData.channels?.let { DmxTransportConfig(it.start, it.end) }
                         else -> null
                     }
 
-                    controllerData.getOrPut(controllerId) { arrayListOf() }
-                        .add(
-                            FixtureMapping(
-                                modelEntity, pixelCount, pixelLocations,
-                                surfaceData.fixtureConfig, transportConfig
-                            )
-                        )
+                    val fixtureMapping = FixtureMapping(
+                        modelEntity, pixelCount, pixelLocations,
+                        entityData.fixtureConfig, transportConfig
+                    )
+                    add(controllerId, fixtureMapping)
                 } catch (e: Exception) {
                     logger.warn(e) { "Skipping $entityName." }
                 }
             }
         }
+
+        model.generateFixtureMappings().forEach { (controllerId, fixtureMappings) ->
+            fixtureMappings.forEach { fixtureMapping ->
+                add(controllerId, fixtureMapping)
+            }
+        }
+    }
+
+    private fun add(controllerId: ControllerId, fixtureMapping: FixtureMapping) {
+        controllerData.getOrPut(controllerId) { arrayListOf() }
+            .add(fixtureMapping)
     }
 
     fun dataForController(controllerId: ControllerId): List<FixtureMapping> =
