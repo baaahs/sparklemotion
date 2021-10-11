bb942025203939e11b46ea59f15c24073d5e0a8e
Christian Williams <antixian666@gmail.com>
2021-10-11 02:49:29 -0700

    Insufficient visual cleanup of BeatLink control.

--- src/jsMain/kotlin/baaahs/plugin/beatlink/BeatLinkControl.kt
+++ src/jsMain/kotlin/baaahs/plugin/beatlink/BeatLinkControl.kt
@@ -21,8 +21,7 @@ import styled.StyleSheet
 import kotlin.math.roundToInt
 
 /**language=glsl*/
-private val beatLinkShaderGlsl = """
-    precision mediump float;
+private val beatLinkShaderGlsl = """precision mediump float;
 
     #define PI 3.14159
 
@@ -36,7 +35,7 @@ private val beatLinkShaderGlsl = """
         float beatsPerMeasure;
         float confidence;
     };
-    uniform RawBeatInfo rawBeatInfo; // @@baaahs.BeatLink:RawBeatInfo
+    uniform RawBeatInfo rawBeatInfo;// @@baaahs.BeatLink:RawBeatInfo
 
     float secPerBeat = rawBeatInfo.beatIntervalMs / 1000.;
 
@@ -44,17 +43,19 @@ private val beatLinkShaderGlsl = """
     const vec2 beatsTopRight = vec2(.9, .9);
     const vec2 beatsDimen = beatsTopRight - beatsBottomLeft;
 
-    const vec3 backgroundColor = vec3(0.75, .56, 0.5);
-    const vec3 beatPowerColor = vec3(0., 0., .4);
+    const vec3 backgroundColor = vec3(0.1, .6, 0.);
+    const vec3 beatPowerColor = vec3(0., 0., .5);
     const vec3 beatIntensityColor = vec3(1., 0., .0);
 
-    const vec3 nowBeatIntensityColor = vec3(0., 1.0, .0);
-
+    const vec4 nowBeatIntensityColor = vec4(0., 1., 0., 1.);
+    const vec3 nowBeatWideBandColor = vec3(1., 1., 0.);
+    const vec3 borderColor = vec3(.4, 0., 0.);
 
     float beatIntensity_(float power) {
         return clamp(
             pow(sin((power * 1. + .55) * PI), 4.) * 1.25 + .0,
-            0., 1.);
+            0., 1.
+        );
     }
 
     float eq(float a, float b) {
@@ -69,9 +70,23 @@ private val beatLinkShaderGlsl = """
         return 1. - v;
     }
 
+    float rect(vec2 bottomLeft, vec2 topRight, vec2 pos) {
+        if (step(pos, beatsBottomLeft) == vec2(0.) && step(beatsTopRight, pos) == vec2(0.)) {
+            return 1.;
+        } else {
+            return 0.;
+        }
+    }
+
     vec3 background(vec2 pos) {
         return backgroundColor - vec3(not(pos.y) * .5);
     }
+
+    float timeInMeasure;
+    float beatInMeasure;
+    float timeInBeat;
+    float nowPower;
+    float nowBeatIntensity;
 
     vec4 drawBeats(vec2 pos) {
         float pX = pos.x * rawBeatInfo.beatsPerMeasure;
@@ -80,20 +95,25 @@ private val beatLinkShaderGlsl = """
         float linearBeatPower = 1. - mod(pX, 1.);
         float linearBeatIntensity = beatIntensity_(linearBeatPower);
 
-        float timeInMeasure = mod((time - rawBeatInfo.measureStartTime) / secPerBeat, rawBeatInfo.beatsPerMeasure);
-        float beatInMeasure = floor(timeInMeasure);
-        float timeInBeat = mod(timeInMeasure, 1.);
-        float nowPower = timeInBeat;
-        float nowBeatIntensity = beatIntensity_(nowPower);
-
         vec3 color = vec3(0.);
         float o = 1.;
 
-        // Beat-tracking bar.
-        float nowBeatXBand = step(abs(pX - nowPower - beatInMeasure), .1);
-        float nowBeatIntensityMarker = step(0., abs(pY - linearBeatIntensity) / .05) * nowBeatXBand * (nowBeatIntensity*.5 + step(nowBeatIntensity, 1.-pY)*.5);
-        color += nowBeatIntensityMarker * nowBeatIntensityColor;
-        o -= nowBeatIntensityMarker;
+        // Beat-tracking bar, wide channel.
+        float nowBeatWideBandMarker = step(abs(pX - nowPower - beatInMeasure), .1);
+
+        // Beat-tracking bar, red slider LED.
+        float wideBandHotLine = nowBeatWideBandMarker * step(abs(pY - nowBeatIntensity), .025);
+        color += o * wideBandHotLine * beatIntensityColor.rgb;
+        o -= o * wideBandHotLine;
+
+        // Beat-tracking bar, gooey indicator center.
+        float nowBeatXBand = step(abs(pX - nowPower - beatInMeasure), .04);
+        float nowBeatIntensityMarker = nowBeatXBand * nowBeatIntensityColor.a;
+        color += o * nowBeatIntensityMarker * nowBeatIntensityColor.rgb * step(pY, nowBeatIntensity);
+        o -= o * nowBeatIntensityMarker;
+
+        color += o * nowBeatWideBandColor * nowBeatWideBandMarker;
+        o -= o * nowBeatWideBandMarker;
 
         // Sine wave-ish intensity preview.
         float beatIntensityBand = 1. - smoothstep(1.0 - distance(1.-linearBeatIntensity + pY - .5, .5), 1.0, 0.99);
@@ -114,19 +134,24 @@ private val beatLinkShaderGlsl = """
 
     void main(void) {
         vec2 pos = gl_FragCoord.xy / resolution.xy;
+        timeInMeasure = mod((time - rawBeatInfo.measureStartTime) / secPerBeat, rawBeatInfo.beatsPerMeasure);
+        beatInMeasure = floor(timeInMeasure);
+        timeInBeat = mod(timeInMeasure, 1.);
+        nowPower = timeInBeat;
+        nowBeatIntensity = beatIntensity_(nowPower);
+    
+        vec3 color = vec3(0.);
+        float o = 1.;
 
-        if (step(pos, beatsBottomLeft) == vec2(0.) && step(beatsTopRight, pos) == vec2(0.)) {
-            gl_FragColor = drawBeats((pos - beatsBottomLeft) / beatsDimen);
-        } else {
-            float color = 0.0;
-            color += sin(pos.x * cos(time / 15.0) * 80.0) + cos(pos.y * cos(time / 15.0) * 10.0);
-            color += sin(pos.y * sin(time / 10.0) * 40.0) + cos(pos.x * sin(time / 25.0) * 40.0);
-            color += sin(pos.x * sin(time / 5.0) * 10.0) + sin(pos.y * sin(time / 35.0) * 80.0);
-            color *= sin(time / 10.0) * 0.5;
+        // Draw border.
+        float border = 1. - rect(beatsBottomLeft, beatsTopRight, pos);
+        color += border * borderColor * nowBeatIntensity;
+        o -= o * border;
+    
+        // Draw beats.
+        color += o * drawBeats((pos - beatsBottomLeft) / beatsDimen).rgb;
 
-            gl_FragColor = vec4(vec3(color, color * 0.5, sin(color + time / 3.0) * 0.75), 1.0);
-        }
-
+        gl_FragColor = vec4(color, 1.);
     }
 """.trimIndent()
 private val beatLinkVisualizerShader = Shader("BeatLink Visualizer", beatLinkShaderGlsl)
@@ -168,8 +193,8 @@ private val beatLinkControl = xComponent<BeatLinkControlProps>("BeatLinkControl"
         div(+Styles.card) {
             shaderPreview {
                 attrs.shader = beatLinkVisualizerShader
-                attrs.width = 200.px
-                attrs.height = 100.px
+                attrs.width = 300.px
+                attrs.height = 200.px
                 attrs.dumpShader = true
             }
 
