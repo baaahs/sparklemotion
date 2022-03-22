2d1c9632d15a9cbb926b509ff51e31f2756d10e3
Christian Williams <antixian666@gmail.com>
2022-03-21 22:20:37 -0700

    Persist client app mode.

--- src/jsMain/kotlin/baaahs/app/settings/UiSettings.kt
+++ src/jsMain/kotlin/baaahs/app/settings/UiSettings.kt
@@ -1,10 +1,12 @@
 package baaahs.app.settings
 
+import baaahs.app.ui.AppMode
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UiSettings(
     val darkMode: Boolean = false,
     val useSharedContexts: Boolean = true,
-    val renderButtonPreviews: Boolean = false
+    val renderButtonPreviews: Boolean = false,
+    val appMode: AppMode = AppMode.Show
 )
