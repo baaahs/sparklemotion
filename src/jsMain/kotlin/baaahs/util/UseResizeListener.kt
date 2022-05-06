5349e857c404b86f017c3c7e8562ab6b441cbf74
Christian Williams <antixian666@gmail.com>
2022-05-06 14:02:13 -0700

    Suppress spurious resize refreshes.

--- src/jsMain/kotlin/baaahs/util/UseResizeListener.kt
+++ src/jsMain/kotlin/baaahs/util/UseResizeListener.kt
@@ -3,20 +3,24 @@ package baaahs.util
 import external.ResizeObserver
 import external.lodash.throttle
 import org.w3c.dom.Element
-import react.RBuilder
-import react.RefObject
-import react.useCallback
-import react.useEffect
+import react.*
 
+@Suppress("unused")
 fun RBuilder.useResizeListener(elementRef: RefObject<out Element>, onResized: () -> Unit) {
     val onResizedThrottled = useCallback(elementRef, callback = throttle(onResized, 40))
+
+    val previousSize = useRef<Pair<Int, Int>>(null)
 
     // Fire the callback anytime the ref is resized
     useEffect {
         val element = elementRef.current ?: return@useEffect
 
         val ro = ResizeObserver { _, _ ->
-            onResizedThrottled()
+            val size = element.clientWidth to element.clientHeight
+            if (previousSize.current != size) {
+                previousSize.current = size
+                onResizedThrottled()
+            }
         }
         ro.observe(element)
 
