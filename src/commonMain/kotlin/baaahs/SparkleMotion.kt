ec30a24d17d33ccc13666dc7f0db2c49148c744f
Christian Williams <antixian666@gmail.com>
2022-05-09 18:53:27 -0700

    Fix layering bug causing dimmed-out buttons in button groups.
    See https://www.paulirish.com/2012/why-moving-elements-with-translate-is-better-than-posabs-topleft/ for background.

--- src/commonMain/kotlin/baaahs/SparkleMotion.kt
+++ src/commonMain/kotlin/baaahs/SparkleMotion.kt
@@ -9,5 +9,9 @@ object SparkleMotion {
 
     const val EXTRA_ASSERTIONS = true
 
+    // This theoretically _may_ (but doesn't appear to, on Chrome at least) slow down client framerate.
+    // See https://www.paulirish.com/2012/why-moving-elements-with-translate-is-better-than-posabs-topleft/
+    const val USE_CSS_TRANSFORM = false
+
     val logger = Logger<SparkleMotion>()
 }
