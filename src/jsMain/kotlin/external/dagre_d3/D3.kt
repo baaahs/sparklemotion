75aa36eff08fd835f8c1109a566d090028afc4f2
Christian Williams <antixian666@gmail.com>
2022-05-21 13:05:39 -0700

    Semi-working DAG visualization!

---
+++ src/jsMain/kotlin/external/dagre_d3/D3.kt
@@ -0,0 +1,5 @@
+@file:JsModule("d3")
+
+package external.dagre_d3
+
+external fun select(arg: dynamic): dynamic
---
+++ src/jsMain/kotlin/external/dagre_d3/Dagre.kt
@@ -0,0 +1,6 @@
+@file:JsModule("dagre-d3")
+
+package external.dagre_d3
+
+external fun render()
+external fun layout(graph: Graph)
