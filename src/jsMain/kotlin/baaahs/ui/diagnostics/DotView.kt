7706a1cd38168809fe2d43d097ee408f9782eb00
Christian Williams <antixian666@gmail.com>
2022-08-23 12:48:52 -0700

    More WIP

--- src/jsMain/kotlin/baaahs/ui/diagnostics/DotView.kt
+++ src/jsMain/kotlin/baaahs/ui/diagnostics/DotView.kt
@@ -7,23 +7,40 @@ import baaahs.sim.ui.Styles
 import baaahs.ui.and
 import baaahs.ui.unaryPlus
 import baaahs.ui.xComponent
-import react.Props
-import react.RBuilder
-import react.RHandler
+import mui.material.Checkbox
+import mui.material.FormControl
+import mui.material.FormControlLabel
+import react.*
 import react.dom.code
 import react.dom.div
 import react.dom.pre
-import react.useContext
 
 private val DotView = xComponent<DotProps>("Dot", isPure = true) { props ->
     val appContext = useContext(appContext)
     val styles = appContext.allStyles.uiComponents
 
+    var includePatchMods by state { false }
+    val handleChangeIncludePatchMods by switchEventHandler { _, checked ->
+        includePatchMods = checked
+    }
+
     val linkedProgram = props.linkedProgram
+
+    FormControl {
+        FormControlLabel {
+            attrs.label = buildElement { +"Include Patch Mods" }
+            attrs.control = buildElement {
+                Checkbox {
+                    attrs.checked = includePatchMods
+                    attrs.onChange = handleChangeIncludePatchMods
+                }
+            }
+        }
+    }
 
     div(+Styles.contentDiv and styles.codeContainer) {
         pre(+styles.code) {
-            Dag().apply { visit(props.fixtureType, linkedProgram) }.text
+            Dag(includePatchMods).apply { visit(props.fixtureType, linkedProgram) }.text
                 .split("\n").forEach { line ->
                     code { +line; +"\n" }
                 }
