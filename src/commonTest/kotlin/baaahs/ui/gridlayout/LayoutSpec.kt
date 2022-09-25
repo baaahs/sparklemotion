65c60fcb3c8674963f6053b519852212ed80c315
Christian Williams <antixian666@gmail.com>
2022-09-24 19:08:44 -0700

    GridLayout: Fix some bugs in port from react-grid-layout.

--- src/commonTest/kotlin/baaahs/ui/gridlayout/LayoutSpec.kt
+++ src/commonTest/kotlin/baaahs/ui/gridlayout/LayoutSpec.kt
@@ -1,18 +1,67 @@
 package baaahs.ui.gridlayout
 
 import baaahs.describe
+import baaahs.toEqual
+import baaahs.ui.gridlayout.CompactType.Companion.determineFrom
+import ch.tutteli.atrium.api.verbs.expect
 import org.spekframework.spek2.Spek
 
 object LayoutSpec : Spek({
     describe<Layout> {
-        val items by value {
-            listOf(
-                LayoutItem(0, 0, 1, 1, "A")
-            )
+        val layout by value {
+            """
+                ABC.
+                DEFG
+                .HI.
+            """.trimIndent().toLayout()
         }
-        val layout by value { Layout(items, 1, 1) }
-        context("moveElement") {
 
+        val move by value {
+            { id: String, x: Int, y: Int ->
+                val item = layout.find(id)!!
+                val compactType = item.determineFrom(x, y)
+                layout.moveElement(item, x, y, false, compactType)
+            }
+        }
+
+        it("moving A one space down shifts D down") {
+            expect(move("A", 0, 1).stringify()).toEqual("""
+                    .BC.
+                    AEFG
+                    DHI.
+                """.trimIndent())
+        }
+
+        it("moving A two spaces down leaves the rest undisturbed") {
+            expect(move("A", 0, 2).stringify()).toEqual("""
+                    .BC.
+                    DEFG
+                    AHI.
+                """.trimIndent())
+        }
+
+        it("moving A one space right shifts B and C over, because there's room to the right") {
+            expect(move("A", 1, 0).stringify()).toEqual("""
+                    .ABC
+                    DEFG
+                    .HI.
+                """.trimIndent())
+        }
+
+        it("moving A two spaces right shifts C over") {
+            expect(move("A", 2, 0).stringify()).toEqual("""
+                    .BAC
+                    DEFG
+                    .HI.
+                """.trimIndent())
+        }
+
+        it("moving D one space right shifts E into its place") {
+            expect(move("D", 1, 1).stringify()).toEqual("""
+                    ABC.
+                    EDFG
+                    .HI.
+                """.trimIndent())
         }
     }
 })
