5a96d7294a22c7b76f70febdaddcccbc93e0275d
Christian Williams <antixian666@gmail.com>
2022-09-27 14:33:21 -0700

    Draw slider handle as an SVG.

--- src/jsMain/kotlin/baaahs/app/ui/gadgets/slider/HandleView.kt
+++ src/jsMain/kotlin/baaahs/app/ui/gadgets/slider/HandleView.kt
@@ -1,7 +1,6 @@
 package baaahs.app.ui.gadgets.slider
 
 import baaahs.app.ui.appContext
-import baaahs.ui.and
 import baaahs.ui.slider.*
 import baaahs.ui.unaryPlus
 import baaahs.ui.xComponent
@@ -13,14 +12,14 @@ import react.RHandler
 import react.dom.div
 import react.dom.events.KeyboardEvent
 import react.dom.events.PointerEvent
+import react.dom.img
 import react.dom.onKeyDown
 import react.dom.onPointerDown
-import react.dom.setProp
 import react.useContext
 import styled.inlineStyles
 import web.html.HTMLElement
 
-val handle = xComponent<HandleProps>("Handle") { props ->
+private val handle = xComponent<HandleProps>("Handle") { props ->
     val appContext = useContext(appContext)
     val styles = appContext.allStyles.gadgetsSlider
     val sliderContext = useContext(sliderContext)
@@ -58,25 +57,8 @@ val handle = xComponent<HandleProps>("Handle") { props ->
 
         attrs.onPointerDown = sliderContext.getPointerDownHandlerFor(handle)
         attrs.onKeyDown = sliderContext.getKeyDownHandlerFor(handle)
-    }
 
-    div(+styles.handleWrapper) {
-        ref = handleRef
-        setProp("role", "slider")
-        setProp("aria-valuemin", sliderContext.domain.start)
-        setProp("aria-valuemax", sliderContext.domain.endInclusive)
-        setProp("aria-valuenow", value)
-        inlineStyles {
-            top = percent.pct
-        }
-
-        div(+styles.handleNotch) {}
-        div(+styles.handleNotch) {}
-        div(+styles.handleNotch) {}
-        div(+styles.handleNotch and styles.handleNotchMiddle) {}
-        div(+styles.handleNotch and styles.handleNotchLower) {}
-        div(+styles.handleNotch and styles.handleNotchLower) {}
-        div(+styles.handleNotch and styles.handleNotchLower) {}
+        img(src = "/assets/slider-handle-full.svg", classes = +styles.handleNormal) {}
     }
 }
 
