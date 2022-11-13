0bbdb362c78691d63ca2116bbb7daa2b3fd3d19c
Christian Williams <antixian666@gmail.com>
2022-11-13 14:48:17 -0800

    Add description, author, and tags to Shader model.

--- src/commonMain/kotlin/baaahs/libraries/ShaderLibraryManager.kt
+++ src/commonMain/kotlin/baaahs/libraries/ShaderLibraryManager.kt
@@ -1,13 +1,15 @@
 package baaahs.libraries
 
 import baaahs.PubSub
-import baaahs.camelize
 import baaahs.gl.Toolchain
+import baaahs.gl.openShader
+import baaahs.hyphenize
 import baaahs.io.Fs
 import baaahs.mapper.Storage
 import baaahs.show.Shader
 import baaahs.sm.webapi.SearchShaderLibraries
 import baaahs.sm.webapi.Topics
+import baaahs.util.Logger
 import baaahs.util.UniqueIds
 import kotlinx.serialization.modules.SerializersModule
 
@@ -30,7 +32,7 @@ class ShaderLibraryManager(
         }).searchShaderLibraries) { command ->
             val matches = arrayListOf<ShaderLibrary.Entry>()
             shaderLibraries.forEach { (_, shaderLibrary) ->
-                val libId = shaderLibrary.title.camelize()
+                val libId = shaderLibrary.title.hyphenize()
                 shaderLibrary.entries.forEach { entry ->
                     if (entry.matches(command.terms)) {
                         matches.add(entry.copy(id = "${libId}:${entry.id}"))
@@ -51,8 +53,14 @@ class ShaderLibraryManager(
         val uniqueIds = UniqueIds<Fs.File>()
 
         val shaderLibraryPath = storage.shaderLibraryPath(libraryName)
-        val entries = shaderLibraryPath.listFiles().mapNotNull { file ->
+        logger.info { "Indexing shader library at \"${shaderLibraryPath.fullPath}\"."}
+        var countOk = 0
+        var countTotal = 0
+        val entries = shaderLibraryPath.listFilesRecursively().mapNotNull { file ->
+            countTotal++
             if (Regex("\\.(png|jpg|gif|webp|txt|md|json)\$").containsMatchIn(file.name))
+                return@mapNotNull null
+            if (file.fs.isDirectory(file))
                 return@mapNotNull null
 
             val fileRelPath = file.relativeTo(shaderLibraryPath)
@@ -60,16 +68,21 @@ class ShaderLibraryManager(
                 val contents = file.read()!!
                 val shader = toolchain.import(contents, file.name)
                 val analysis = toolchain.analyze(shader)
+                val openShader = toolchain.openShader(shader)
 //                println("${file.name}: $analysis")
+                val tags = buildList {
+                    add("@type=${openShader.shaderType.title}")
+                    if (openShader.isFilter) add("@filter")
+                }
                 ShaderLibraryIndexFile.Entry(
-                    uniqueIds.idFor(file) { analysis.shader.suggestId() },
-                    analysis.shader.title,
+                    uniqueIds.idFor(file) { shader.title.hyphenize() },
+                    shader.title,
+                    shader.description,
                     null,
-                    null,
-                    emptyList(),
+                    tags,
                     fileRelPath,
                     analysis.errors.map { it.toString() }
-                )
+                ).also { countOk++ }
             } catch (e: Exception) {
 //                println("Problem analyzing ${file.name}.")
 //                e.printStackTrace()
@@ -89,9 +102,20 @@ class ShaderLibraryManager(
             libraryName,
             null,
             null,
-            entries
+            entries.sortedBy { it.id.lowercase() }
         )
+        logger.info { "Indexed $countOk/$countTotal shaders." }
         storage.saveShaderLibraryIndexFile(libraryName, index)
+    }
+
+    private suspend fun Fs.File.listFilesRecursively(): List<Fs.File> = buildList {
+        listFiles().forEach {
+            if (it.isDir()) {
+                addAll(it.listFilesRecursively())
+            } else {
+                add(it)
+            }
+        }
     }
 
     private suspend fun loadShaderLibraries(): Map<Fs.File, ShaderLibrary> {
@@ -120,5 +144,9 @@ class ShaderLibraryManager(
                 }
             )
         }
+    }
+
+    companion object {
+        private val logger = Logger<ShaderLibraryManager>()
     }
 }
