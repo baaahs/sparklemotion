65c25caa13465e1aecbc59cd6287aec1a75dfc65
Christian Williams <antixian666@gmail.com>
2022-11-15 09:42:10 -0800

    Support #if/elif condition evaluation in GLSL.
    Fixes #376.

--- src/commonMain/kotlin/baaahs/gl/glsl/GlslMacroExpressionEvaluator.kt
+++ src/commonMain/kotlin/baaahs/gl/glsl/GlslMacroExpressionEvaluator.kt
@@ -1,21 +1,14 @@
 package baaahs.gl.glsl
 
 import baaahs.util.Logger
-import com.github.murzagalin.evaluator.Constant
 import com.github.murzagalin.evaluator.Evaluator
 
 object GlslMacroExpressionEvaluator {
+    private val evaluator = Evaluator(emptyList(), emptyList())
+
     fun evaluate(expression: String): Boolean {
-        val evaluator = Evaluator(
-            emptyList(),
-            listOf(
-                Constant("__VERSION__", 330.0)
-            )
-        )
         return evaluator.evaluateBoolean(expression)
-            .also {
-                logger.info { "$expression -> $it" }
-            }
+            .also { logger.debug { "$expression -> $it" } }
     }
 
     private val logger = Logger<GlslMacroExpressionEvaluator>()
