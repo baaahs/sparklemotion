0f29a173732f12ef481bfe7ffe2e68cefc62ea7e
Matt Rucker <vgmatt.rucker@gmail.com>
2023-05-24 12:06:07 -0700

    WIP

---
+++ src/commonMain/kotlin/baaahs/plugin/midi/MidiData.kt
@@ -0,0 +1,27 @@
+package baaahs.plugin.midi
+
+import baaahs.ui.IObservable
+import baaahs.ui.Observable
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MidiData(
+    val sustainPedalCount: Int = 0,
+    val noteCount: Int = 0
+) {
+    companion object {
+        val UNKNOWN = MidiData(0, 0)
+    }
+}
+
+
+interface MidiSource : IObservable {
+    fun getMidiData(): MidiData
+
+    object None : Observable(), MidiSource {
+        val none = MidiData(0, 0)
+
+        override fun getMidiData(): MidiData = none
+    }
+}
+
