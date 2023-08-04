7ab4d5d86fdcefaa1e37e4184a1e44fd48551ab0
Kevin King <kcking@google.com>
2023-08-04 02:37:16 -0700

    naive attempt at native webcam capture
    - libbridj (the default used by the sarxos webcam lib) doesn't have M1 support, so I added https://github.com/eduramiba/webcam-capture-driver-native as a submodule
    - this currently requires a manual `mvn install`, I wasn't sure how to get this effect with just modifications to the gradle build file
    - I wasn't able to completely validate on my m1 mac, but it did prompt to open my webcam, and the blue light on the webcam turned on
    - I also added the VideoInPlugin to pluggables, but I'm not sure we want it enabled by default. maybe we can only open the webcam when the first frame is pulled, and close it after inactivity
    - make BrainMain.main `suspend`, this appeared to raise errors to the shell instead of failing silently
    
    - openjdk 15 failed to compile webcam-capture-driver-native, upgrading to 20.2 (x86) worked

--- src/jvmMain/kotlin/baaahs/plugin/webcam/DefaultVideoProvider.kt
+++ src/jvmMain/kotlin/baaahs/plugin/webcam/DefaultVideoProvider.kt
@@ -1,16 +1,41 @@
 package baaahs.plugin.webcam
 
+import com.github.eduramiba.webcamcapture.drivers.NativeDriver;
+import com.danielgergely.kgl.IntBuffer
 import com.danielgergely.kgl.TextureResource
 import com.github.sarxos.webcam.Webcam
+import org.lwjgl.opengl.GL11
 
 actual val DefaultVideoProvider: VideoProvider
     get() = WebcamCaptureVideoProvider
 
+
 object WebcamCaptureVideoProvider : VideoProvider {
+    private val webcam: Webcam = run {
+        Webcam.setDriver(NativeDriver())
+        Webcam.getDefault()
+    }
+
+    init {
+        webcam.open()
+    }
+
     override fun getTextureResource(): TextureResource {
-        val webcam = Webcam.getDefault()
-        println("webcam = ${webcam}")
-        error("doink!")
+        val image = webcam.image
+        val vals = IntBuffer(image.width * image.height)
+        for (x in 0..image.width) {
+            for (y in 0..image.height) {
+                vals[x*image.width + y] = image.getRGB(x, y)
+            }
+        }
+
+        return TextureResource(
+            width = image.width,
+            height = image.height,
+            format = GL11.GL_RGB,
+            type = GL11.GL_UNSIGNED_BYTE,
+            data = vals
+        )
     }
 
 }
