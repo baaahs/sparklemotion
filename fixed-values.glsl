4812a14cabe0671d7cef00ef14f839bbd8d38a6f
Aravindo Wingeier <wingeier@adobe.com>
2023-08-05 19:05:14 -0700

    fix sim

--- fixed-values.glsl
+++ fixed-values.glsl
@@ -33,18 +33,30 @@ bool isLeft(){
     return fixtureInfo.position.z < 0.;
 }
 
+
+
+#define SIMULATE_BPM true
+
+float getBPM() {
+    if (SIMULATE_BPM)  {
+        return 120.;
+    } else {
+        return beatInfo.bpm;
+    }
+}
+
 float getTimeOfLastBeat() {
     float beatShift = isLeft() ? 1.0 : 0.;
 
     // SIM:
-    if(false){
-        float bpm =  120.;
+    if(SIMULATE_BPM){
+        float bpm =  getBPM();
         float beatDuration = 60. / bpm;
         float timeSince2Beats = mod(time + beatShift * beatDuration, beatDuration  * 2.);
         return time - timeSince2Beats;
     } else {
         // real:
-        float beatDuration = 60. / beatInfo.bpm;
+        float beatDuration = 60. / getBPM();
         float timeSince2Beats = mod(beatInfo.beat + beatShift, 2.) * beatDuration;
         return time - timeSince2Beats;
     }
@@ -57,13 +69,13 @@ float rand(float seed){
 
 
 float getSeed(){
-    float counter = getTimeOfLastBeat() * beatInfo.bpm / 60.;
+    float counter = getTimeOfLastBeat() * getBPM() / 60.;
     return counter /2.;
 }
 
 
 float pan(float value /* [-1...1]*/ ) {
-    return 3. * PI /2. + value * 1.5*  PI;
+    return 2. * PI /2. + value * 0.7*  PI;
 }
 
 float tilt(float value /* [-1...1]*/) {
@@ -72,16 +84,21 @@ float tilt(float value /* [-1...1]*/) {
 
 float panValue(){
     float offset = isLeft() ? 4. : 0.;
-    int t = int(mod(getSeed() + offset, 8.));
-    if(t == 0) return -1.;
-    else if(t == 1) return -0.4;
-    else if(t == 2) return 0.3;
-    else if(t == 3) return 0.8;
-    else if(t == 4) return 1.;
-    else if(t == 5) return 0.4;
-    else if(t == 6) return 0.1;
-    else if(t == 7) return -0.5;
-    else if(t == 7) return -0.8;
+    int t = int(mod(getSeed() + offset, 13.));
+    if(t == 0)      return -0.4;
+    else if(t == 1) return -0.3;
+    else if(t == 2) return -0.4;
+    else if(t == 3) return -0.2;
+    else if(t == 4) return 0.;
+    else if(t == 5) return 0.3;
+    else if(t == 6) return 0.2;
+    else if(t == 7) return 0.4;
+    else if(t == 8) return 0.25;
+    else if(t == 9) return 0.3;
+    else if(t == 10) return 0.;
+    else if(t == 11) return -0.1;
+    else if(t == 12) return -0.25;
+
     return 0. ;
 }
 
