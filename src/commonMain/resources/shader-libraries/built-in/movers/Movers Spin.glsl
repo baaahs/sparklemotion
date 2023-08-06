10eab446b73904100a66c15b1aa096777c2dfa3c
Aravindo Wingeier <wingeier@adobe.com>
2023-08-06 15:29:49 -0700

    spin

--- src/commonMain/resources/shader-libraries/built-in/movers/Movers Spin.glsl
+++ src/commonMain/resources/shader-libraries/built-in/movers/Movers Spin.glsl
@@ -47,8 +47,10 @@ void main(in MovingHeadParams inHead, out MovingHeadParams params) {
     params = inHead;
 
     bool isLeft = fixtureInfo.position.z < 0.;
+
+    // turns end-to-end with some wiggle in between. see https://graphtoy.com/?f1(x,t)=0.5%20+%20(cos(10*x/3.)+sin(10*x/2.))%20/%202.0%20/%202.0&v1=true&f2(x,t)=&v2=true&f3(x,t)=&v3=true&f4(x,t)=&v4=true&f5(x,t)=&v5=false&f6(x,t)=&v6=true&grid=1&coords=2.8945805231935795,0.2949890979050782,6.157897418768507
     float rotation = 0.5 + (cos(speed*time/3.)+sin(speed*time/2.)) / 2.0 / 2.0;// [0...1]
-    params.pan = mix(0.0, (2.) * PI, mirrored && isLeft ? rotation : 1. - rotation) + (isLeft ? offset : 0.);
+    params.pan = mix(0.0, 2. * PI, mirrored && isLeft ? rotation : 1. - rotation) + (isLeft ? offset : 0.);
 
     params.tilt = .3+(1. + cos(1. + time))/2.0;
 }
