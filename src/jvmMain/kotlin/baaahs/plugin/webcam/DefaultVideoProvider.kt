d3e6a1c9ebc39ea72cfbc657cfac7d0de45f8b54
Christian Williams <antixian666@gmail.com>
2023-08-15 17:34:14 -0700

    Fix buffer overruns etc.

--- src/jvmMain/kotlin/baaahs/plugin/webcam/DefaultVideoProvider.kt
+++ src/jvmMain/kotlin/baaahs/plugin/webcam/DefaultVideoProvider.kt
@@ -1,8 +1,9 @@
 package baaahs.plugin.webcam
 
-import com.github.eduramiba.webcamcapture.drivers.NativeDriver;
-import com.danielgergely.kgl.IntBuffer
+import baaahs.Color
+import com.danielgergely.kgl.ByteBuffer
 import com.danielgergely.kgl.TextureResource
+import com.github.eduramiba.webcamcapture.drivers.NativeDriver
 import com.github.sarxos.webcam.Webcam
 import org.lwjgl.opengl.GL11
 
@@ -22,13 +23,17 @@ object WebcamCaptureVideoProvider : VideoProvider {
 
     override fun getTextureResource(): TextureResource {
         val image = webcam.image
-        val vals = IntBuffer(image.width * image.height)
-        var maxPix = 0;
-        for (x in 0..image.width) {
-            for (y in 0..image.height) {
+        val vals = ByteBuffer(image.width * image.height * 3)
+        var maxPix = 0
+        for (y in 0 until image.height) {
+            for (x in 0 until image.width) {
                 val pixel = image.getRGB(x, y)
-                maxPix = Integer.max(maxPix, pixel)
-                vals[x*image.width + y] = pixel
+                maxPix = Integer.max(maxPix and 0xFFFFFF, pixel and 0xFFFFFF)
+                val color = Color(pixel)
+                val bufOffset = (y * image.width + x) * 3
+                vals[bufOffset + 0] = color.redB
+                vals[bufOffset + 1] = color.greenB
+                vals[bufOffset + 2] = color.blueB
             }
         }
 
