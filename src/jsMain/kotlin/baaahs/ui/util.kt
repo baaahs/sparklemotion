53323d53c4f6fb30be3ae2378cc469619e2e650d
Christian Williams <antixian666@gmail.com>
2023-11-14 13:43:29 -0800

    Cleanup.

--- src/jsMain/kotlin/baaahs/ui/util.kt
+++ src/jsMain/kotlin/baaahs/ui/util.kt
@@ -2,6 +2,7 @@ package baaahs.ui
 
 import baaahs.context2d
 import baaahs.document
+import js.core.jso
 import kotlinext.js.getOwnPropertyNames
 import kotlinx.css.*
 import kotlinx.css.properties.Time
@@ -232,9 +233,9 @@ fun renderWrapper(block: RBuilder.() -> Unit): View {
 fun buildElements(handler: Render): ReactNode =
     react.buildElements(RBuilder(), handler)
 
-val preventDefault: (web.events.Event) -> Unit = { event -> event.preventDefault() }
+val preventDefault: (Event) -> Unit = { event -> event.preventDefault() }
 val disableScroll = {
-    document.body.addEventListener(TouchEvent.TOUCH_MOVE, preventDefault, js("{ passive: false }"))
+    document.body.addEventListener(TouchEvent.TOUCH_MOVE, preventDefault, jso { passive = false })
 }
 val enableScroll = {
     document.body.removeEventListener(TouchEvent.TOUCH_MOVE, preventDefault)
