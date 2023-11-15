eab7392e72564a5dfd3e1097f5c9d81409c0477d
Christian Williams <antixian666@gmail.com>
2023-11-15 12:32:05 -0800

    Update slider tracks in real time during slider adjustment.

--- src/jsMain/kotlin/baaahs/app/ui/gadgets/slider/Track.kt
+++ src/jsMain/kotlin/baaahs/app/ui/gadgets/slider/Track.kt
@@ -4,6 +4,7 @@ import baaahs.app.ui.appContext
 import baaahs.ui.mixin
 import baaahs.ui.unaryPlus
 import baaahs.ui.xComponent
+import external.react_compound_slider.BetterSliderItem
 import external.react_compound_slider.GetTrackProps
 import external.react_compound_slider.SliderItem
 import kotlinx.css.height
@@ -15,6 +16,7 @@ import react.RHandler
 import react.dom.div
 import react.useContext
 import styled.inlineStyles
+import web.html.HTMLElement
 import kotlin.math.max
 import kotlin.math.min
 
@@ -22,9 +24,25 @@ private val track = xComponent<TrackProps>("Track") { props ->
     val appContext = useContext(appContext)
     val styles = appContext.allStyles.gadgetsSlider
 
-    val upper = min(props.source.percent, props.target.percent)
-    val lower = max(props.source.percent, props.target.percent)
+    val source = props.source
+    val target = props.target
+    val trackRef = ref<HTMLElement>()
+    val drawTrack = callback(source, target) {
+        trackRef.current?.let {
+            val upper = min(source.percent, target.percent)
+            val lower = max(source.percent, target.percent)
+            it.style.top = upper.pct.toString()
+            it.style.height = (lower.toFloat() - upper.toFloat()).pct.toString()
+        }
+    }
+    if (source is BetterSliderItem) observe(source) { drawTrack() }
+    if (target is BetterSliderItem) observe(target) { drawTrack() }
+
+    val upper = min(source.percent, target.percent)
+    val lower = max(source.percent, target.percent)
     div(+styles.track) {
+        ref = trackRef
+
         inlineStyles {
             top = upper.pct
             height = (lower.toFloat() - upper.toFloat()).pct
