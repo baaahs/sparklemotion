624bfab4cb7fde20b98be8dc76b171cbcda8097b
Christian Williams <antixian666@gmail.com>
2024-07-26 17:08:40 -0400

    Make baaahs.ui.DialogHolder a concrete common interface.
    Working around a bug in recent-ish kotlin compiler versions that reports missing class files for expect/actual classes.

--- src/jsMain/kotlin/baaahs/ui/DialogHolder.kt
+++ src/jsMain/kotlin/baaahs/ui/DialogHolder.kt
@@ -2,16 +2,10 @@ package baaahs.ui
 
 import react.RBuilder
 
-actual interface DialogHolder {
-    actual fun showDialog(view: View)
-    actual fun showMenuDialog(title: String, options: List<DialogMenuItem>)
-    actual fun closeDialog()
-
-    fun showDialog(block: RBuilder.() -> Unit) {
-        showDialog(object : JsView {
-            override fun RBuilder.render() {
-                block()
-            }
-        })
-    }
+fun DialogHolder.showDialog(block: RBuilder.() -> Unit) {
+    showDialog(object : JsView {
+        override fun RBuilder.render() {
+            block()
+        }
+    })
 }
--- src/jvmMain/kotlin/baaahs/ui/View.kt
+++ src/jvmMain/kotlin/baaahs/ui/View.kt
@@ -1,13 +1,3 @@
 package baaahs.ui
 
-actual interface View
-
-actual interface Icon
-
-actual interface DialogHolder {
-    actual fun showDialog(view: View)
-    actual fun showMenuDialog(title: String, options: List<DialogMenuItem>)
-    actual fun closeDialog()
-}
-
 actual fun confirm(message: String): Boolean = TODO("confirm not implemented")
