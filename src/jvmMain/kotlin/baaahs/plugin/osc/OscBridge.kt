5419dccb5376e65f274091667db829d0d467a08c
Tom G Varik <2086264+tgvarik@users.noreply.github.com>
2024-08-27 19:01:01 -0700

    parse sonic runway messages to color palette

--- src/jvmMain/kotlin/baaahs/plugin/osc/OscBridge.kt
+++ src/jvmMain/kotlin/baaahs/plugin/osc/OscBridge.kt
@@ -8,36 +8,35 @@ import com.illposed.osc.transport.OSCPort
 import com.illposed.osc.transport.OSCPortIn
 
 actual class OscBridge {
-    private val oscPort: Int = OSCPort.DEFAULT_SC_OSC_PORT
+    private val oscPort      : Int = 7777
+    private val palette      : Array<baaahs.Color> = Array(5) { baaahs.Color.BLACK }
+    private val addressRegex : Regex = Regex("Color([0-4])_([rgb])", RegexOption.IGNORE_CASE)
+    private val selector     : OSCPatternAddressMessageSelector = OSCPatternAddressMessageSelector("/SonicRunway/Color?_?")
 
-    private lateinit var receiver: OSCPortIn
-
+    private lateinit var receiver     : OSCPortIn
     actual fun start() {
-        println("OscBridge: OSC starting...")
         receiver = OSCPortIn(oscPort)
 
-
-        /*
-         * OSCMessageListener listener = new OSCMessageListener() {
- *   public void acceptMessage(OSCMessageEvent event) {
- *     System.out.println("Message received: " + event.getMessage().getAddress());
- *   }
- * };
- * MessageSelector selector = new OSCPatternAddressMessageSelector(
- *     "/message/receiving");
- * receiver.getDispatcher().addListener(selector, listener);
- * // NOTE You might want to use this code, in case you have bundles
- * //      with time-stamps in the future, which you still want
- * //      to process immediately.
- * //receiver.getDispatcher().setAlwaysDispatchingImmediately(true);
- * receiver.startListening();
-         */
-
         val listener = OSCMessageListener { event ->
-            println("OscBridge: ${event.message.toString()}")
+            val result = addressRegex.find(event.message.address)
+            if (result != null) {
+                val (index, component) = result.destructured
+                val idx = index.toInt()
+                val value : Byte = (255.0 * event.message.arguments[0] as Float).toInt().toByte()
+                when(component) {
+                    "r" -> palette[idx] = palette[idx].withRed(value)
+                    "g" -> palette[idx] = palette[idx].withGreen(value)
+                    "b" -> palette[idx] = palette[idx].withBlue(value)
+                }
+
+                println("OscBridge: ${event.time} : [${idx}] ${palette[idx]}")
+            }
         }
 
-        receiver.dispatcher.addListener(OSCPatternAddressMessageSelector("*"), listener)
+
+        receiver.isResilient = true
+        receiver.dispatcher.addListener(selector, listener)
+        receiver.dispatcher.isAlwaysDispatchingImmediately = true
         receiver.startListening()
         println("OscBridge: OSC started! $receiver")
     }
@@ -46,3 +45,5 @@ actual class OscBridge {
         private val logger = Logger<OscBridge>()
     }
 }
+
+// 192.168.0.31 255:255:255:0 port 2390
