748c0c2a6d6c54ae83375ab32e625597f6df29ba
Tom G Varik <2086264+tgvarik@users.noreply.github.com>
2024-08-27 21:18:36 -0700

    add change tracking

--- src/jvmMain/kotlin/baaahs/plugin/osc/OscBridge.kt
+++ src/jvmMain/kotlin/baaahs/plugin/osc/OscBridge.kt
@@ -1,10 +1,8 @@
 package baaahs.plugin.osc
 
-import baaahs.plugin.sound_analysis.AnalyzerStream
 import baaahs.util.Logger
 import com.illposed.osc.OSCMessageListener
 import com.illposed.osc.messageselector.OSCPatternAddressMessageSelector
-import com.illposed.osc.transport.OSCPort
 import com.illposed.osc.transport.OSCPortIn
 
 actual class OscBridge {
@@ -12,8 +10,27 @@ actual class OscBridge {
     private val palette      : Array<baaahs.Color> = Array(5) { baaahs.Color.BLACK }
     private val addressRegex : Regex = Regex("Color([0-4])_([rgb])", RegexOption.IGNORE_CASE)
     private val selector     : OSCPatternAddressMessageSelector = OSCPatternAddressMessageSelector("/SonicRunway/Color?_?")
-
     private lateinit var receiver     : OSCPortIn
+
+    private fun updateColor(idx : Int, k : String, value : Byte) : Boolean {
+        var oldVal : Byte = 0
+        when (k) {
+            "r" -> {
+                oldVal = palette[idx].redB
+                palette[idx] = palette[idx].withRed(value)
+            }
+            "g" -> {
+                oldVal = palette[idx].greenB
+                palette[idx] = palette[idx].withGreen(value)
+            }
+            "b" -> {
+                oldVal = palette[idx].blueB
+                palette[idx] = palette[idx].withBlue(value)
+            }
+        }
+        return oldVal != value
+    }
+
     actual fun start() {
         receiver = OSCPortIn(oscPort)
 
@@ -23,13 +40,10 @@ actual class OscBridge {
                 val (index, component) = result.destructured
                 val idx = index.toInt()
                 val value : Byte = (255.0 * event.message.arguments[0] as Float).toInt().toByte()
-                when(component) {
-                    "r" -> palette[idx] = palette[idx].withRed(value)
-                    "g" -> palette[idx] = palette[idx].withGreen(value)
-                    "b" -> palette[idx] = palette[idx].withBlue(value)
+                if (updateColor(idx, component, value)) {
+                    // TODO: Send it to SparkleMotion
+                    println("OscBridge: ${event.time} : [${idx}] ${palette[idx]}")
                 }
-
-                println("OscBridge: ${event.time} : [${idx}] ${palette[idx]}")
             }
         }
 
