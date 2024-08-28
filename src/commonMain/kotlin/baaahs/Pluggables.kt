1336f8c3d62ba7a353bbe97af2a693adaacb02c9
Christian Williams <antixian666@gmail.com>
2024-08-27 21:35:12 -0700

    Merge tgvarik's OSC implementation for Sonic Runway.

--- src/commonMain/kotlin/baaahs/Pluggables.kt
+++ src/commonMain/kotlin/baaahs/Pluggables.kt
@@ -2,12 +2,14 @@ package baaahs
 
 import baaahs.plugin.Plugin
 import baaahs.plugin.beatlink.BeatLinkPlugin
+import baaahs.plugin.osc.OscPlugin
 import baaahs.plugin.sonic_runway.SonicRunwayPlugin
 import baaahs.plugin.sound_analysis.SoundAnalysisPlugin
 import baaahs.plugin.webcam.VideoInPlugin
 
 object Pluggables {
     val plugins = listOf<Plugin<*>>(
+        OscPlugin,
         BeatLinkPlugin,
         SoundAnalysisPlugin,
         VideoInPlugin,
