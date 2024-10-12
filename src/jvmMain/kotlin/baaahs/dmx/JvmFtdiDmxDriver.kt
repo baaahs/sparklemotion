224fccee98b9afbcd2ef0c88316d30bb16fcc78c
Christian Williams <antixian666@gmail.com>
2024-10-11 18:04:37 -0700

    Switch to org.bidib.com.ftdi:FTD2xxJ for native DMX library.

--- src/jvmMain/kotlin/baaahs/dmx/JvmFtdiDmxDriver.kt
+++ src/jvmMain/kotlin/baaahs/dmx/JvmFtdiDmxDriver.kt
@@ -1,12 +1,11 @@
 package baaahs.dmx
 
-import com.ftdichip.ftd2xx.DeviceType
-import com.ftdichip.ftd2xx.Service
+import com.ftdi.FTDevice
 
 object JvmFtdiDmxDriver : Dmx.Driver {
     override fun findDmxDevices(): List<Dmx.Device> {
         return try {
-            Service.listDevicesByType(DeviceType.FT_DEVICE_232R).map { JvmDmxDevice(it) }
+            FTDevice.getDevices().map { JvmDmxDevice(it) }
         } catch (e: UnsatisfiedLinkError) {
             DmxManagerImpl.logger.warn { "DMX driver not found, DMX will be disabled." }
             e.printStackTrace()
