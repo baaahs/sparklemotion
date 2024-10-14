312ce5419f5f905a9518183a08364dd861adc500
Christian Williams <antixian666@gmail.com>
2024-10-14 14:50:21 -0700

    No more 'by getting' in build files.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -51,7 +51,7 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             kotlin.srcDirs(file(project.layout.buildDirectory.file("generated/ksp/metadata/commonMain/kotlin").get()))
 
             dependencies {
@@ -64,7 +64,7 @@ kotlin {
                 implementation(projects.rpc)
             }
         }
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(kotlin("test"))
 //                implementation("io.insert-koin:koin-test:${Versions.koin}")
@@ -75,7 +75,7 @@ kotlin {
             }
         }
 
-        val jvmMain by getting {
+        jvmMain {
             dependencies {
                 implementation(libs.kotlinxCoroutinesDebug)
                 implementation(libs.kotlinxCli)
@@ -123,7 +123,7 @@ kotlin {
                 implementation(libs.webcamCaptureDriverNative)
             }
         }
-        val jvmTest by getting {
+        jvmTest {
             dependencies {
                 implementation(project.dependencies.platform("org.junit:junit-bom:${Versions.junit}"))
                 implementation(libs.kotestRunnerJunit5)
@@ -136,7 +136,7 @@ kotlin {
             }
         }
 
-        val jsMain by getting {
+        jsMain {
             dependencies {
                 implementation(libs.kotlinxHtmlJs)
 
@@ -181,7 +181,7 @@ kotlin {
                 implementation(npm("d3-array", "^3.2.4"))
             }
         }
-        val jsTest by getting {
+        jsTest {
             dependencies {
                 implementation(kotlin("test-js"))
             }
