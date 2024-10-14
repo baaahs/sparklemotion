1de8f627c8ba3bb2cfe48b5057ea1db439e46caa
Christian Williams <antixian666@gmail.com>
2024-10-14 14:12:14 -0700

    Extract CommonPinkyMain.

--- shared/src/jvmMain/kotlin/baaahs/sm/server/PinkyMain.kt
+++ shared/src/jvmMain/kotlin/baaahs/sm/server/PinkyMain.kt
@@ -1,5 +1,6 @@
 package baaahs.sm.server
 
+import baaahs.CommonPinkyMain
 import baaahs.Pinky
 import baaahs.Pluggables
 import baaahs.di.JvmPinkyModule
@@ -9,8 +10,6 @@ import baaahs.gl.GlBase
 import baaahs.io.Fs
 import baaahs.io.RealFs
 import baaahs.net.JvmNetwork
-import baaahs.sm.brain.ProdBrainSimulator
-import baaahs.util.KoinLogger
 import baaahs.util.Logger
 import baaahs.util.SystemClock
 import io.ktor.http.*
@@ -21,66 +20,42 @@ import io.ktor.server.plugins.callloging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.pipeline.*
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
-import org.koin.dsl.koinApplication
 import java.nio.file.Path
-import kotlin.system.exitProcess
 
-@ObsoleteCoroutinesApi
 fun main(args: Array<String>) {
     PinkyMain(args).run()
 }
 
-@ObsoleteCoroutinesApi
-class PinkyMain(private val args: Array<String>) {
-    private val logger by lazy { Logger("PinkyMain") }
+class PinkyMain(private val args: Array<String>) : CommonPinkyMain() {
+    override val logger by lazy { Logger<PinkyMain>() }
 
     fun run() {
         GlBase.manager // First thing, we need to wake up OpenGL on the main thread.
 
-        logger.info { "Are you pondering what I'm pondering?" }
-        logger.info { "Running JVM ${System.getProperty("java.vendor")} ${System.getProperty("java.version")} from ${System.getProperty("java.home")}." }
-
-        val programName = PinkyMain::class.simpleName ?: "Pinky"
+        val programName = this::class.simpleName ?: "Pinky"
         val clock = SystemClock
-        val pinkyInjector = koinApplication {
-            logger(KoinLogger())
+        val modules = listOf(
+            PluginsModule(Pluggables.plugins).getModule(),
+            JvmPlatformModule(clock).getModule(),
+            JvmPinkyModule(programName, args).getModule()
+        )
 
-            modules(
-                PluginsModule(Pluggables.plugins).getModule(),
-                JvmPlatformModule(clock).getModule(),
-                JvmPinkyModule(programName, args).getModule()
-            )
-        }
-
-        val pinkyScope = pinkyInjector.koin.createScope<Pinky>()
-        val pinky = pinkyScope.get<Pinky>()
-        configureKtor(pinky, pinkyScope)
-
-        logger.info { responses.random() }
-
-        try {
-            val pinkyArgs = pinkyScope.get<PinkyArgs>()
-            runBlocking(pinkyScope.get<CoroutineDispatcher>(named("PinkyMainDispatcher"))) {
-                pinky.startAndRun {
-                    if (pinkyArgs.simulateBrains) {
-                        pinkyScope.get<ProdBrainSimulator>().enableSimulation()
-                    }
-                }
-            }
-        } catch (e: Throwable) {
-            logger.error(e) { "Failed to start Pinky." }
-        } finally {
-            logger.info { "Exiting." }
-            exitProcess(1)
-        }
+        bootstrap(modules)
     }
 
-    private fun configureKtor(pinky: Pinky, pinkyScope: Scope) {
+    override fun systemId(): String =
+        "JVM ${System.getProperty("java.vendor")} ${System.getProperty("java.version")}"
+
+    override fun homeDir(): String =
+        System.getProperty("java.home")
+
+    override fun exitProcess(code: Int) {
+        kotlin.system.exitProcess(code)
+    }
+
+    override fun configureKtor(pinky: Pinky, pinkyScope: Scope) {
         val ktor = (pinky.httpServer as JvmNetwork.RealLink.KtorHttpServer)
 
         ktor.application.install(CallLogging)
@@ -107,16 +82,6 @@ class PinkyMain(private val args: Array<String>) {
         val file = call.resolveResource(path, resourcePackage)
         if (file is OutgoingContent)
             call.respond(HttpStatusCode.OK, file)
-    }
-
-    companion object {
-        private val responses = listOf(
-            "I think so, Brain, but Lederhosen won't stretch that far.",
-            "Yeah, but I thought Madonna already had a steady bloke!",
-            "I think so, Brain, but what would goats be doing in red leather turbans?",
-            "I think so, Brain... but how would we ever determine Sandra Bullock's shoe size?",
-            "Yes, Brain, I think so. But how do we get Twiggy to pose with an electric goose?"
-        )
     }
 }
 
