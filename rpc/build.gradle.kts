df360238e732c8ac63491af04ee85682b2887572
Christian Williams <antixian666@gmail.com>
2024-10-14 17:05:38 -0700

    Fix dependency notation.

--- rpc/build.gradle.kts
+++ rpc/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+                implementation(libs.kotlinxSerializationJson)
             }
         }
     }
