4a5a57fbf10c8a86eaa5b932e0002689dbdcd370
Christian Williams <antixian666@gmail.com>
2024-10-16 00:37:43 -0700

    Revert "Extract CommonPinkyMain."
    This reverts commit 820afd0d6f3fa53b5874764589e58eade0abb297.

--- shared/src/jvmMain/kotlin/baaahs/sm/server/PinkyMain.kt
+++ shared/src/jvmMain/kotlin/baaahs/sm/server/PinkyMain.kt
@@ -1,6 +1,6 @@
 package baaahs.sm.server
 
-import baaahs.CommonPinkyMain
+import baaahs.Pinky
 import baaahs.Pluggables
 import baaahs.di.JvmPinkyModule
 import baaahs.di.JvmPlatformModule
@@ -8,8 +8,10 @@ import baaahs.di.PluginsModule
 import baaahs.gl.GlBase
 import baaahs.io.Fs
 import baaahs.io.RealFs
+import baaahs.net.JvmNetwork
 import baaahs.net.KtorHttpServer
-import baaahs.net.Network
+import baaahs.sm.brain.ProdBrainSimulator
+import baaahs.util.KoinLogger
 import baaahs.util.Logger
 import baaahs.util.SystemClock
 import io.ktor.http.*
@@ -20,49 +22,74 @@ import io.ktor.server.plugins.callloging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.pipeline.*
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
+import org.koin.dsl.koinApplication
 import java.nio.file.Path
+import kotlin.system.exitProcess
 
+@ObsoleteCoroutinesApi
 fun main(args: Array<String>) {
     PinkyMain(args).run()
 }
 
-class PinkyMain(private val args: Array<String>) : CommonPinkyMain() {
-    override val logger by lazy { Logger<PinkyMain>() }
+@ObsoleteCoroutinesApi
+class PinkyMain(private val args: Array<String>) {
+    private val logger by lazy { Logger("PinkyMain") }
 
     fun run() {
         GlBase.manager // First thing, we need to wake up OpenGL on the main thread.
 
-        val programName = this::class.simpleName ?: "Pinky"
+        logger.info { "Are you pondering what I'm pondering?" }
+        logger.info { "Running JVM ${System.getProperty("java.vendor")} ${System.getProperty("java.version")} from ${System.getProperty("java.home")}." }
+
+        val programName = PinkyMain::class.simpleName ?: "Pinky"
         val clock = SystemClock
-        val modules = listOf(
-            PluginsModule(Pluggables.plugins).getModule(),
-            JvmPlatformModule(clock).getModule(),
-            JvmPinkyModule(programName, args).getModule()
-        )
+        val pinkyInjector = koinApplication {
+            logger(KoinLogger())
 
-        bootstrap(modules)
+            modules(
+                PluginsModule(Pluggables.plugins).getModule(),
+                JvmPlatformModule(clock).getModule(),
+                JvmPinkyModule(programName, args).getModule()
+            )
+        }
+
+        val pinkyScope = pinkyInjector.koin.createScope<Pinky>()
+        val pinky = pinkyScope.get<Pinky>()
+        configureKtor(pinky, pinkyScope)
+
+        logger.info { responses.random() }
+
+        try {
+            val pinkyArgs = pinkyScope.get<PinkyArgs>()
+            runBlocking(pinkyScope.get<CoroutineDispatcher>(named("PinkyMainDispatcher"))) {
+                pinky.startAndRun {
+                    if (pinkyArgs.simulateBrains) {
+                        pinkyScope.get<ProdBrainSimulator>().enableSimulation()
+                    }
+                }
+            }
+        } catch (e: Throwable) {
+            logger.error(e) { "Failed to start Pinky." }
+        } finally {
+            logger.info { "Exiting." }
+            exitProcess(1)
+        }
     }
 
-    override fun systemId(): String =
-        "JVM ${System.getProperty("java.vendor")} ${System.getProperty("java.version")}"
+    private fun configureKtor(pinky: Pinky, pinkyScope: Scope) {
+        val ktor = (pinky.httpServer as KtorHttpServer)
 
-    override fun homeDir(): String =
-        System.getProperty("java.home")
-
-    override fun exitProcess(code: Int) {
-        kotlin.system.exitProcess(code)
-    }
-
-    override fun configureKtorApplication(httpServer: Network.HttpServer, pinkyScope: Scope) {
-        val application = (httpServer as KtorHttpServer).application
-        application.install(CallLogging)
+        ktor.application.install(CallLogging)
 
         val dataDir = pinkyScope.get<Fs>().resolve(".")
         val firmwareDir = pinkyScope.get<Fs.File>(named("firmwareDir"))
 
-        application.routing {
+        ktor.application.routing {
             get("") { call.respondRedirect("/ui/") }
             get("monitor") { call.respondRedirect("/monitor/") }
             get("midi") { call.respondRedirect("midi/") }
@@ -81,6 +108,16 @@ class PinkyMain(private val args: Array<String>) : CommonPinkyMain() {
         val file = call.resolveResource(path, resourcePackage)
         if (file is OutgoingContent)
             call.respond(HttpStatusCode.OK, file)
+    }
+
+    companion object {
+        private val responses = listOf(
+            "I think so, Brain, but Lederhosen won't stretch that far.",
+            "Yeah, but I thought Madonna already had a steady bloke!",
+            "I think so, Brain, but what would goats be doing in red leather turbans?",
+            "I think so, Brain... but how would we ever determine Sandra Bullock's shoe size?",
+            "Yes, Brain, I think so. But how do we get Twiggy to pose with an electric goose?"
+        )
     }
 }
 
