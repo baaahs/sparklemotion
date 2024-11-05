d6250ae317a0ced098f97cc7588a852f38088296
Christian Williams <antixian666@gmail.com>
2024-11-04 21:28:02 -0800

    Oops no need for a jni confguration.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -49,10 +49,6 @@ repositories {
 group = "org.baaahs"
 version = "0.0.1"
 
-configurations {
-    create("jni")
-}
-
 fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
