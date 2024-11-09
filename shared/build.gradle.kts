729de059281d1d28a62da6ca9e6ae5ea67340cda
Christian Williams <antixian666@gmail.com>
2024-11-08 23:28:00 -0800

    Add webpack bundle analysis.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -214,6 +214,8 @@ kotlin {
 
                 // Used by slider view:
                 implementation(npm("d3-array", "^3.2.4"))
+
+                implementation(devNpm("webpack-bundle-analyzer", "4.10.2"))
             }
         }
         jsTest {
