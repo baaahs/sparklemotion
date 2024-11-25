4520ea452debcc17bd7072b6dd34f2fd0e84c126
Christian Williams <antixian666@gmail.com>
2024-11-25 11:45:43 -0800

    Gadget updates must happen on the Pinky Main context to protect against race conditions.

--- shared/src/commonMain/kotlin/baaahs/sm/server/GadgetManager.kt
+++ shared/src/commonMain/kotlin/baaahs/sm/server/GadgetManager.kt
@@ -4,23 +4,33 @@ import baaahs.Gadget
 import baaahs.GadgetDataSerializer
 import baaahs.PubSub
 import baaahs.util.Clock
+import baaahs.util.Logger
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlin.concurrent.Volatile
 import kotlin.coroutines.CoroutineContext
 
 class GadgetManager(
     private val pubSub: PubSub.Server,
     private val clock: Clock,
-    private val coroutineContext: CoroutineContext
+    private val pinkyMainContext: CoroutineContext
 ) {
     private val gadgets: MutableMap<String, Gadget> = mutableMapOf()
+
+    @Volatile
     var lastUserInteraction = clock.now()
 
     fun <T : Gadget> registerGadget(id: String, gadget: T) {
         val topic =
             PubSub.Topic("/gadgets/$id", GadgetDataSerializer)
         val channel = pubSub.publish(topic, gadget.state) { updated ->
-            lastUserInteraction = clock.now()
+            CoroutineScope(pinkyMainContext + CoroutineName("Gadget Update Handler")).launch {
+                lastUserInteraction = clock.now()
 
-            gadget.applyState(updated)
+                logger.debug { "Gadget \"$id\" updated to: $updated" }
+                gadget.applyState(updated)
+            }
         }
         gadget.listen { channel.onChange(it.state) }
         gadgets[id] = gadget
@@ -30,5 +40,9 @@ class GadgetManager(
         @Suppress("UNCHECKED_CAST")
         return (gadgets[id]
             ?: error("no such gadget \"$id\" among [${gadgets.keys.sorted()}]")) as T
+    }
+
+    companion object {
+        private val logger = Logger<GadgetManager>()
     }
 }
