412482776fa266bd844c819a1162e0d6050eb52a
Christian Williams <antixian666@gmail.com>
2024-11-25 10:46:24 -0800

    Disable webpack bundle analyzer.

--- shared/webpack.config.d/bundleAnalyzer.js
+++ shared/webpack.config.d/bundleAnalyzer.js
@@ -1,10 +1,10 @@
-// file: /path/to/your/module/webpack.config.d/bundleAnalyzer.js
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-config.plugins.push(
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      generateStatsFile: true,
-    }),
-)
+// // file: /path/to/your/module/webpack.config.d/bundleAnalyzer.js
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+//
+// config.plugins.push(
+//     new BundleAnalyzerPlugin({
+//       analyzerMode: 'static',
+//       openAnalyzer: false,
+//       generateStatsFile: true,
+//     }),
+// )
