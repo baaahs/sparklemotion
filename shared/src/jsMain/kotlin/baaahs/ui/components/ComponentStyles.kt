bdb20a1bc9d9f2c7856285b2bed56daf4603b0ac
Christian Williams <antixian666@gmail.com>
2024-12-04 15:24:28 -0800

    More entity editor polish.

--- shared/src/jsMain/kotlin/baaahs/ui/components/ComponentStyles.kt
+++ shared/src/jsMain/kotlin/baaahs/ui/components/ComponentStyles.kt
@@ -95,6 +95,14 @@ class ListAndDetailStyles(val theme: Theme) : StyleSheet("ui-components-list-and
         display = Display.flex
         justifyContent = JustifyContent.center
         overflow = Overflow.auto
+
+        ".MuiFormControl-root:first-child" {
+            marginTop = 0.px
+
+            ".MuiPaper-root:first-child" {
+                paddingTop = 1.em
+            }
+        }
     }
 
     val detailHeader by css {
