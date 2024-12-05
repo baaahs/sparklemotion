89c79f0532508af2bbec56c2945267f43f2e62dd
Christian Williams <antixian666@gmail.com>
2024-12-05 01:08:31 -0800

    Tighten up app drawer for mono-doc mode.

--- shared/src/jsMain/kotlin/baaahs/app/ui/Styles.kt
+++ shared/src/jsMain/kotlin/baaahs/app/ui/Styles.kt
@@ -188,7 +188,7 @@ class ThemeStyles(val theme: Theme) : StyleSheet("app-ui-theme", isStatic = true
     }
     val titleFooter by css {
         position = Position.absolute
-        top = 2.2.em
+        bottom = 2.px
         fontSize = .6.em
         opacity = .6
         textWrap = TextWrap.nowrap
@@ -292,15 +292,31 @@ class ThemeStyles(val theme: Theme) : StyleSheet("app-ui-theme", isStatic = true
     }
 
     val appDrawerHeader by css {
+        position = Position.relative
         display = Display.flex
         alignItems = Align.center
         padding = Padding(theme.spacing(0, 1).toLinearDimension())
         mixIn(theme.mixins.toolbar)
         justifyContent = JustifyContent.flexEnd
+        userSelect = UserSelect.none
     }
 
     val appModeTab by css {
         minWidth = 0.px
+    }
+
+    val appDrawerLogotype by css {
+        position = Position.absolute
+        fontWeight = FontWeight.bold.important
+        fontSize = 1.2.rem.important
+        left = 1.em
+    }
+    val appDrawerLogotypeSub by css {
+        position = Position.absolute
+        fontWeight = FontWeight.lighter.important
+        fontSize = .6.rem.important
+        left = 9.em
+        top = 3.em
     }
 
     val appDrawerDocInfo by css {
