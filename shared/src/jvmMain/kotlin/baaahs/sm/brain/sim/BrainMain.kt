8c094fd5b830ca93ab52ce4790d8639dcbe7bff2
Christian Williams <antixian666@gmail.com>
2024-12-05 14:20:25 -0800

    AWT Brain simulator cleanup.

--- shared/src/jvmMain/kotlin/baaahs/sm/brain/sim/BrainMain.kt
+++ shared/src/jvmMain/kotlin/baaahs/sm/brain/sim/BrainMain.kt
@@ -1,12 +1,9 @@
 package baaahs.sm.brain.sim
 
-import baaahs.Color
-import baaahs.client.document.sceneStore
 import baaahs.io.RealFs
-import baaahs.model.Model
 import baaahs.net.JvmNetwork
 import baaahs.plugin.Plugins
-import baaahs.sm.brain.proto.Pixels
+import baaahs.sm.brain.sim.JvmPixelsDisplay.Layout
 import baaahs.util.SystemClock
 import baaahs.util.globalLaunch
 import kotlinx.cli.ArgParser
@@ -14,16 +11,7 @@ import kotlinx.cli.ArgType
 import kotlinx.cli.default
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import java.awt.Canvas
-import java.awt.Dimension
-import java.awt.Frame
-import java.awt.Graphics
 import java.io.File
-import kotlin.math.ceil
-import kotlin.math.min
-import kotlin.math.roundToInt
-import kotlin.math.sqrt
-import kotlin.random.Random
 
 suspend fun main(args: Array<String>) {
     val argParser = ArgParser(BrainMain::class.simpleName ?: "Brain")
@@ -43,8 +31,9 @@ class BrainMain(private val args: Args) {
 
         val network = JvmNetwork()
         val brainId = args.brainId ?: JvmNetwork.myAddress.toString()
+        val startingLayout = Layout(20, 20)
         val brainSimulator = BrainSimulator(
-            brainId, network, JvmPixelsDisplay(2000), SystemClock, CoroutineScope(Dispatchers.Default)
+            brainId, network, JvmPixelsDisplay(startingLayout).pixels, SystemClock, CoroutineScope(Dispatchers.Default)
         )
 
 //        val mySurface = if (args.anonymous) {
@@ -74,68 +63,3 @@ class BrainMain(private val args: Args) {
     }
 }
 
-class JvmPixelsDisplay(pixelCount: Int) : Pixels {
-    override val size = pixelCount
-    private val colors = Array(size) { Color.BLACK }
-    private val pixelsPerRow = ceil(sqrt(size.toFloat())).roundToInt()
-    private val pixelsPerCol = pixelsPerRow
-
-    private val frame = Frame("Pixels!")
-    private val canvas = PanelCanvas()
-
-    inner class PanelCanvas : Canvas() {
-        override fun paint(g: Graphics?) {
-            g?.apply {
-                val doubleBuffer = createImage(width, height)
-                val bufG: Graphics = doubleBuffer.graphics
-                bufG.color = java.awt.Color.BLACK
-                bufG.clearRect(0, 0, width, height)
-
-                for (i in 0 until this@JvmPixelsDisplay.size) {
-                    val row = i % pixelsPerRow
-                    val col = i / pixelsPerRow
-
-                    val pixWidth = width / pixelsPerCol
-                    val pixHeight = height / pixelsPerRow
-                    val pixGap = if (pixWidth > 3) 2 else if (pixWidth > 1) 1 else 0
-
-                    val color = colors[i]
-                    bufG.color = java.awt.Color(color.rgb)
-
-                    bufG.fillRect(
-                        col * pixWidth, row * pixHeight,
-                        pixWidth - pixGap, pixHeight - pixGap
-                    )
-                }
-
-                g.drawImage(doubleBuffer, 0, 0, this@PanelCanvas)
-            }
-        }
-    }
-
-    init {
-        frame.size = Dimension(300, 300)
-        frame.isVisible = true
-
-        canvas.preferredSize = frame.size
-        canvas.background = java.awt.Color.BLACK
-        frame.add(canvas)
-        frame.pack()
-        frame.invalidate()
-    }
-
-    override fun get(i: Int): Color = colors[i]
-
-    override fun set(i: Int, color: Color) {
-        colors[i] = color
-    }
-
-    override fun set(colors: Array<Color>) {
-        val pixCount = min(colors.size, size)
-        colors.copyInto(this.colors, 0, 0, pixCount)
-    }
-
-    override fun finishedFrame() {
-        canvas.repaint()
-    }
-}
