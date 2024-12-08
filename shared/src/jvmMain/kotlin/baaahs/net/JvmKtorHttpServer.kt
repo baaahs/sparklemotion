ae7730415280b965c26cce8702680393dfec3bcb
Christian Williams <antixian666@gmail.com>
2024-12-08 00:42:31 -0800

    Cleanup.

--- shared/src/jvmMain/kotlin/baaahs/net/JvmKtorHttpServer.kt
+++ shared/src/jvmMain/kotlin/baaahs/net/JvmKtorHttpServer.kt
@@ -1,19 +1,19 @@
 package baaahs.net
 
 import baaahs.io.Fs
-import baaahs.sm.server.ExceptionReporter
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import kotlinx.coroutines.CoroutineScope
 import java.io.File
+import java.time.Duration
 
 class JvmKtorHttpServer(
     link: Network.Link,
@@ -24,9 +24,9 @@ class JvmKtorHttpServer(
         // Let's give brains lots of time for OTA download:
 //                responseWriteTimeoutSeconds = 3000
     }) {
-        install(WebSockets.Plugin) {
-            pingPeriod = java.time.Duration.ofSeconds(15)
-            timeout = java.time.Duration.ofSeconds(15)
+        install(WebSockets) {
+            pingPeriod = Duration.ofSeconds(15)
+            timeout = Duration.ofSeconds(15)
             maxFrameSize = Long.Companion.MAX_VALUE
             masking = false
         }
