e8e2023e870fe168ea97fd2669d7b86c99c6faad
Christian Williams <antixian666@gmail.com>
2025-04-09 21:50:26 -0700

    Bump kotlin-wrappers to 1.0.0-pre.859.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val lwjgl = "3.3.1"
-    const val kotlinWrappers = "1.0.0-pre.853"
+    const val kotlinWrappers = "1.0.0-pre.859"
 
     // Test:
     const val junit = "5.11.3"
